fbfce16e2122b38c51612102e30a162aa49116c3
ishibashikeita <ishibashi.hnut@gmail.com>
2024-10-09 10:35:33 +0900

    delete: 不要分の削除

--- lib/View/Component/Parts/user_info_tile.dart
+++ lib/View/Component/Parts/user_info_tile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_text.dart';
