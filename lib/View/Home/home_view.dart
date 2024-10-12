9c96f1e69f1e1c06f42938310f8208e0c3efb31c
ishibashikeita <ishibashi.hnut@gmail.com>
2024-10-12 12:04:59 +0900

    fix: 不要分の修正

--- lib/View/Home/home_view.dart
+++ lib/View/Home/home_view.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_scaffold.dart';
-import 'package:unicorn_flutter/View/Component/Parts/health_check_button.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return CustomScaffold(
+    return const CustomScaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            HealthCheckButton(onTap: () {}, aiCheck: false),
+            Text('HomeView'),
           ],
         ),
       ),
