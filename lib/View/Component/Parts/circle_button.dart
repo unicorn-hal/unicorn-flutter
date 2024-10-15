b895a3f6995fe5ba19de6e38455f9b77176d397a
ryudra131 <ryunosuke.13043702@gmail.com>
2024-10-15 16:39:25 +0900

    fix: GestureDetector・FractionallySizedBoxへの記法変更、引数名変更

--- lib/View/Component/Parts/circle_button.dart
+++ lib/View/Component/Parts/circle_button.dart
@@ -3,41 +3,35 @@ import 'package:flutter/material.dart';
 class CircleButton extends StatelessWidget {
   const CircleButton({
     super.key,
-    this.isbuttonFilledColor = false,
-    this.buttonFilledColor = Colors.white,
-    this.imageSize = 100,
+    required this.buttonSize,
+    required this.buttonColor,
     required this.onTap,
     required this.icon,
   });
 
-  // isbuttonFilledColorは不要であれば削除
-  final bool isbuttonFilledColor;
-  // isbuttonFilledColorをtrue -> buttonFilledColorに色指定して背景色がその指定した色に変化
-  // isbuttonFilledColorをfalse -> デフォの灰色の背景色
-  final Color buttonFilledColor;
-  final double imageSize;
+  final double buttonSize;
+  final Color buttonColor;
+  final Icon icon;
   final Function onTap;
-  final Image icon;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () => onTap(),
-      style: ElevatedButton.styleFrom(
-        elevation: 0,
-        fixedSize: Size(imageSize, imageSize),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(imageSize / 2)
-        ),
-        backgroundColor: isbuttonFilledColor ? buttonFilledColor : Colors.grey,
-      ),
+    return GestureDetector(
+      onTap: onTap(),
       child: Container(
-        width: imageSize / 2,
-        height: imageSize / 2,
-        color: Colors.white.withOpacity(1.0),
-        child: FittedBox(
-          fit: BoxFit.cover,
-          child: icon,
+        decoration: BoxDecoration(
+          color: buttonColor,
+          shape: BoxShape.circle
+        ),
+        width: buttonSize,
+        height: buttonSize,
+        child: FractionallySizedBox(
+          widthFactor: 0.6,
+          heightFactor: 0.6,
+          child: FittedBox(
+            fit: BoxFit.cover,
+            child: icon,
+          ),
         ),
       ),
     );
