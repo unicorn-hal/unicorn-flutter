687b369063d342277659b2a618f1d5167e0a9e39
kakeru-ikeda <kakerumail1451@gmail.com>
2024-10-20 22:59:21 +0900

    fix: カスタムクラスを利用せず、Package由来のPosition型で扱うように変更

--- lib/Service/Package/Location/location_service.dart
+++ lib/Service/Package/Location/location_service.dart
@@ -1,6 +1,5 @@
 import 'package:permission_handler/permission_handler.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:unicorn_flutter/Model/Entiry/location.dart';
 import 'package:unicorn_flutter/Service/Log/log_service.dart';
 import 'package:unicorn_flutter/Service/Package/PermissionHandler/permission_handler_service.dart';
 
@@ -9,7 +8,7 @@ class LocationService {
       PermissionHandlerService();
 
   /// Geolocatorから現在地情報を取得する基礎サービス
-  Future<Position?> _getPosition() async {
+  Future<Position?> getPosition() async {
     try {
       bool permission = await permissionHandlerService
           .checkAndRequestPermission(Permission.location);
@@ -23,17 +22,5 @@ class LocationService {
       Log.echo('Error: $e');
       return null;
     }
-  }
-
-  /// 現在地情報をカスタムクラスで返却する
-  Future<Location?> getLocation() async {
-    Position? position = await _getPosition();
-    if (position == null) {
-      return null;
-    }
-    return Location.fromJson({
-      'lat': position.latitude,
-      'lng': position.longitude,
-    });
   }
 }
