48154c87db20a67d63b7db7b2fe2bd29d5a71522
school\rei20 <nakada0404rei@gmail.com>
2024-10-22 16:59:07 +0900

    fix: FamilyEmailEditViewデザイン修正

--- lib/View/Profile/FamilyEmail/family_email_edit_view.dart
+++ lib/View/Profile/FamilyEmail/family_email_edit_view.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:unicorn_flutter/View/Component/CustomWidget/custom_scaffold.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_text.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_textfield.dart';
 import 'package:unicorn_flutter/View/Component/Parts/user_image_circle.dart';
@@ -13,56 +16,140 @@ class FamilyEmailEditView extends StatefulWidget {
 class _FamilyEmailEditViewState extends State<FamilyEmailEditView> {
   TextEditingController lastNameController = TextEditingController();
   TextEditingController firstNameController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController telephoneNumberController = TextEditingController();
+  // todo: todo: controller出来たらcontrollerに移動
   @override
   Widget build(BuildContext context) {
     double deviceWidth = MediaQuery.of(context).size.width;
     double deviceHeight = MediaQuery.of(context).size.height;
-    return Container(
-      width: deviceWidth,
-      height: deviceHeight,
-      child: Column(
-        children: [
-          UserImageCircle(
-            imageSize: 150,
-          ),
-          Row(
-            children: [
-              CustomText(text: '姓'),
-              CustomText(text: '名'),
-            ],
-          ),
-          SizedBox(
-            width: deviceWidth,
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                SizedBox(
-                  width: deviceWidth * 0.4,
-                  height: 60,
-                  child: CustomTextfield(
-                    hintText: 'のりた',
-                    backgroundcolor: Colors.white,
-                    controller: lastNameController,
-                    height: 50,
-                    maxLines: 1,
-                  ),
-                ),
-                SizedBox(
-                  width: deviceWidth * 0.4,
-                  height: 60,
-                  child: CustomTextfield(
-                    hintText: 'しおき',
-                    backgroundcolor: Colors.white,
-                    controller: firstNameController,
-                    height: 50,
-                    maxLines: 1,
-                  ),
-                ),
-              ],
+    String imageUrl = 'aaaaaaaaaaaaaaaaaaaaaa';
+    // todo: todo: controller出来たら削除
+    return CustomScaffold(
+      isScrollable: true,
+      body: SizedBox(
+        width: deviceWidth,
+        height: deviceHeight,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 30,
+              ),
+              child: UserImageCircle(
+                imageSize: 150,
+                imageUrl: imageUrl,
+              ),
             ),
-          ),
-        ],
+            SizedBox(
+              width: deviceWidth * 0.9,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(
+                          top: 20,
+                          bottom: 10,
+                          left: 10,
+                        ),
+                        child: CustomText(text: '姓'),
+                      ),
+                      SizedBox(
+                        width: deviceWidth * 0.43,
+                        height: 60,
+                        child: CustomTextfield(
+                          hintText: '山田',
+                          controller: lastNameController,
+                          height: 50,
+                          maxLines: 1,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(
+                          top: 20,
+                          bottom: 10,
+                          left: 10,
+                        ),
+                        child: CustomText(text: '名'),
+                      ),
+                      SizedBox(
+                        width: deviceWidth * 0.43,
+                        height: 60,
+                        child: CustomTextfield(
+                          hintText: '太郎',
+                          controller: firstNameController,
+                          height: 50,
+                          maxLines: 1,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: deviceWidth * 0.9,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      top: 20,
+                      bottom: 10,
+                      left: 10,
+                    ),
+                    child: CustomText(text: 'メールアドレス'),
+                  ),
+                  SizedBox(
+                    width: deviceWidth * 0.9,
+                    height: 60,
+                    child: CustomTextfield(
+                      hintText: 'sample@sample.com',
+                      controller: emailController,
+                      height: 50,
+                      maxLines: 1,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              width: deviceWidth * 0.9,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(
+                      top: 20,
+                      bottom: 10,
+                      left: 10,
+                    ),
+                    child: CustomText(text: '電話番号'),
+                  ),
+                  SizedBox(
+                    width: deviceWidth * 0.9,
+                    height: 60,
+                    child: CustomTextfield(
+                      hintText: 'ハイフンなしで入力',
+                      controller: telephoneNumberController,
+                      height: 50,
+                      maxLines: 1,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
