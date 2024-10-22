8a624770426666216d421615f9b35340fa93206e
LAPTOP-537VN1FQ\rei20 <nakada0404rei@gmail.com>
2024-10-23 01:15:11 +0900

    add: routerにFamilyEmailページのroute追加

--- lib/Route/routes.dart
+++ lib/Route/routes.dart
@@ -4,4 +4,9 @@ mixin Routes {
   static const String healthCheckup = '/health_checkup';
   static const String chat = '/chat';
   static const String profile = '/profile';
+  static const String profileFamilyEmail = '/profile/family_email';
+  static const String profileFamilyEmailRegister =
+      '/profile/family_email/register';
+  static const String profileFamilyEmailSyncContact =
+      '/profile/family_email/sync_contact';
 }
