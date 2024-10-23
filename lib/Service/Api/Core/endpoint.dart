e57cc9cf893874267023fc527f6bb8fe2d10553c
ryo hasegawa <minecraftnolisio@gmail.com>
2024-10-24 01:40:19 +0900

    add: endpoint doctor 追記

--- lib/Service/Api/Core/endpoint.dart
+++ lib/Service/Api/Core/endpoint.dart
@@ -2,4 +2,5 @@ mixin Endpoint {
   static const String users = 'users';
   static const String accounts = 'accounts';
   static const String hospitals = 'hospitals';
+  static const String doctors = 'doctors';
 }
