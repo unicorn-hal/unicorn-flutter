6c89ecf79a601be0156180f65213aab402d1bb3e
kakeru-ikeda <kakerumail1451@gmail.com>
2024-10-24 02:01:03 +0900

    add: Topicの購読を行う関数とEnumの追加

--- lib/Service/Firebase/CloudMessaging/cloud_messaging_service.dart
+++ lib/Service/Firebase/CloudMessaging/cloud_messaging_service.dart
@@ -1,12 +1,20 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:unicorn_flutter/Constants/Enum/fcm_topic_enum.dart';
 import 'package:unicorn_flutter/Service/Log/log_service.dart';
 
 class FirebaseCloudMessagingService {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
 
+  /// 初期化
   Future<void> initialize() async {
     NotificationSettings premission =
         await _firebaseMessaging.requestPermission();
+
+    await _firebaseMessaging.setForegroundNotificationPresentationOptions(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
 
     if (premission.authorizationStatus == AuthorizationStatus.authorized) {
       Log.echo('User accepted permission');
@@ -18,5 +26,17 @@ class FirebaseCloudMessagingService {
   /// FCMトークンを取得
   Future<String?> getToken() async {
     return await _firebaseMessaging.getToken();
+  }
+
+  /// Topicを購読
+  Future<void> subscribeToTopics(List<FCMTopicEnum> topics) async {
+    for (FCMTopicEnum topic in topics) {
+      final String topicName = FCMTopicType.toStringValue(topic);
+      await _firebaseMessaging.subscribeToTopic(topicName).then((value) {
+        Log.echo('Subscribed to topic: $topicName');
+      }).catchError((error) {
+        Log.echo('Failed to subscribe to topic: $topicName');
+      });
+    }
   }
 }
