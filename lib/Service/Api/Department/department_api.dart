ea1f01ce3e00ac1deca2ee49cbb2b93c113538d3
ryo hasegawa <minecraftnolisio@gmail.com>
2024-10-23 17:15:44 +0900

    add: department_api 追記

--- lib/Service/Api/Department/department_api.dart
+++ lib/Service/Api/Department/department_api.dart
@@ -1,6 +1,21 @@
+import 'package:unicorn_flutter/Model/Entity/Department/department.dart';
+import 'package:unicorn_flutter/Model/Entity/api_response.dart';
 import 'package:unicorn_flutter/Service/Api/Core/api_core.dart';
 import 'package:unicorn_flutter/Service/Api/Core/endpoint.dart';
 
 class DepartmentApi extends ApiCore with Endpoint {
   DepartmentApi() : super(Endpoint.departments);
+
+  /// GET
+  /// 診療科一覧取得
+  Future<List<Department>?> getDepartmentList() async {
+    try {
+      final ApiResponse response = await get();
+      return (response.data['data'] as List)
+          .map((e) => Department.fromJson(e))
+          .toList();
+    } catch (e) {
+      return null;
+    }
+  }
 }
