f2ef56fde580d80dd354499708986cf7ed858ba2
LAPTOP-537VN1FQ\rei20 <nakada0404rei@gmail.com>
2024-10-23 19:27:39 +0900

    fix: actionとonTap周り修正

--- lib/View/Component/Parts/Profile/common_item_tile.dart
+++ lib/View/Component/Parts/Profile/common_item_tile.dart
@@ -5,14 +5,14 @@ class CommonItemTile extends StatelessWidget {
   const CommonItemTile({
     super.key,
     required this.title,
-    required this.action,
+    this.action,
     this.onTap,
     this.verticalPadding = 0,
     this.tileHeight = 50,
     this.fontSize = 14,
   });
   final String title;
-  final Widget action;
+  final Widget? action;
   final Function? onTap;
   final double verticalPadding;
   final double tileHeight;
@@ -23,40 +23,46 @@ class CommonItemTile extends StatelessWidget {
     double deviceWidth = MediaQuery.of(context).size.width;
     return Padding(
       padding: EdgeInsets.symmetric(vertical: verticalPadding),
-      child: Container(
-        width: deviceWidth * 0.9,
-        height: tileHeight,
-        decoration: const BoxDecoration(
-          border: BorderDirectional(
-            bottom: BorderSide(
-              width: 1,
-              color: Colors.grey,
-            ),
-          ),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Expanded(
-              flex: 3,
-              child: Padding(
-                padding: const EdgeInsets.only(left: 5),
-                child: CustomText(
-                  text: title,
-                  fontSize: fontSize,
-                ),
+      child: GestureDetector(
+        onTap: () => onTap?.call(),
+        child: Container(
+          width: deviceWidth * 0.9,
+          height: tileHeight,
+          decoration: const BoxDecoration(
+            border: BorderDirectional(
+              bottom: BorderSide(
+                width: 1,
+                color: Colors.grey,
               ),
             ),
-            Expanded(
-              flex: 1,
-              child: Center(
-                  child: GestureDetector(
-                onTap: () => onTap?.call(),
-                child: action,
-              )),
-            ),
-          ],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 3,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 5),
+                  child: CustomText(
+                    text: title,
+                    fontSize: fontSize,
+                  ),
+                ),
+              ),
+              action != null
+                  ? Expanded(
+                      flex: 1,
+                      child: Center(
+                        child: GestureDetector(
+                          onTap: () => onTap?.call(),
+                          child: action,
+                        ),
+                      ),
+                    )
+                  : Container(),
+            ],
+          ),
         ),
       ),
     );
