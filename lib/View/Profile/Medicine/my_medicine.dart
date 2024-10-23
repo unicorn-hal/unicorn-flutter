eec5cae1cb7814163e07a0e3a10dafda2c7a4ba5
LAPTOP-537VN1FQ\rei20 <nakada0404rei@gmail.com>
2024-10-24 00:00:42 +0900

    add: MyMedicineViewファイル作成

---
+++ lib/View/Profile/Medicine/my_medicine.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyMedicineView extends StatelessWidget {
+  const MyMedicineView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
