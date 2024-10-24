411f4d940d1ff32e2ede853ec3a6852838ef1fae
school\rei20 <nakada0404rei@gmail.com>
2024-10-24 17:24:16 +0900

    fix: RegularMedicineSettingViewデザイン修正

--- lib/View/Profile/Medicine/regular_medicine_setting_view.dart
+++ lib/View/Profile/Medicine/regular_medicine_setting_view.dart
@@ -18,8 +18,8 @@ class RegularMedicineSettingView extends StatefulWidget {
 class _RegularMedicineSettingViewState
     extends State<RegularMedicineSettingView> {
   TextEditingController controller = TextEditingController();
-  bool _value = false;
-  bool appBarAction = true;
+  bool registration = true;
+  bool reminder = false;
   // todo: controller出来たら削除
   @override
   Widget build(BuildContext context) {
@@ -27,8 +27,10 @@ class _RegularMedicineSettingViewState
     return CustomScaffold(
       isScrollable: true,
       appBar: CustomAppBar(
+        title: '常備薬の登録',
+        foregroundColor: Colors.white,
         backgroundColor: ColorName.mainColor,
-        actions: appBarAction
+        actions: registration
             ? [
                 IconButton(
                   onPressed: () {
@@ -37,7 +39,7 @@ class _RegularMedicineSettingViewState
                   },
                   icon: const Icon(
                     Icons.delete_outline,
-                    color: Colors.black,
+                    color: Colors.white,
                   ),
                 ),
               ]
@@ -58,7 +60,30 @@ class _RegularMedicineSettingViewState
                 padding: EdgeInsets.only(
                   left: 5,
                 ),
-                child: CustomText(text: '常備薬のリマインダー設定'),
+                child: CustomText(text: 'おくすりの名称'),
+              ),
+            ),
+            SizedBox(
+              width: deviceWidth * 0.9,
+              height: 70,
+              child: CustomTextfield(
+                hintText: '20文字以内で入力してください',
+                controller: controller,
+                height: 50,
+                maxLines: 1,
+              ),
+            ),
+            Container(
+              width: deviceWidth * 0.9,
+              padding: const EdgeInsets.only(
+                top: 20,
+                bottom: 20,
+              ),
+              child: const Padding(
+                padding: EdgeInsets.only(
+                  left: 5,
+                ),
+                child: CustomText(text: 'おくすりの錠数'),
               ),
             ),
             Padding(
@@ -72,51 +97,80 @@ class _RegularMedicineSettingViewState
                   hintText: 'おくすりの名称',
                   controller: controller,
                   height: 50,
+                  maxLines: 1,
+                  number: true,
                 ),
               ),
             ),
             SizedBox(
               width: deviceWidth * 0.9,
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Row(
-                    children: [
-                      Checkbox(
-                        value: _value,
-                        onChanged: (value) => setState(() => _value = value!),
-                      ),
-                      const CustomText(text: 'リマインドする')
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
+                  const Padding(
+                    padding: EdgeInsets.only(
                       left: 5,
-                      bottom: 20,
                     ),
-                    child: Row(
-                      children: [
-                        CustomDrumRoll(
-                          showTime: false,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        CustomDrumRoll(
-                          showTime: true,
-                        ),
-                      ],
-                    ),
+                    child: CustomText(text: 'リマインダーを設定'),
                   ),
+                  reminder
+                      ? Padding(
+                          padding: const EdgeInsets.only(
+                            top: 10,
+                            bottom: 20,
+                          ),
+                          child: Row(
+                            children: [
+                              IconButton(
+                                onPressed: () {
+                                  reminder = !reminder;
+                                  setState(() {});
+                                },
+                                icon: const Icon(Icons.remove_circle_outlined,
+                                    color: Colors.red),
+                              ),
+                              CustomDrumRoll(
+                                showTime: false,
+                                reservation: DateTime.now(),
+                                // todo: リマインダー設定がすでにある場合reservationに入れる
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              CustomDrumRoll(
+                                showTime: true,
+                                reservation: DateTime.now(),
+                                // todo: リマインダー設定がすでにある場合reservationに入れる
+                              ),
+                            ],
+                          ),
+                        )
+                      : Padding(
+                          padding: const EdgeInsets.only(
+                            top: 10,
+                            bottom: 20,
+                          ),
+                          child: IconButton(
+                            onPressed: () {
+                              reminder = !reminder;
+                              setState(() {});
+                            },
+                            icon: const Icon(
+                              Icons.add,
+                              color: Colors.blue,
+                            ),
+                          ),
+                        ),
                   SizedBox(
                     width: deviceWidth * 0.9,
                     child: CustomButton(
-                      text: '決定',
+                      text: '保存',
                       onTap: () {
                         Navigator.pop(context);
                       },
                       isFilledColor: true,
                     ),
-                  )
+                  ),
                 ],
               ),
             ),
