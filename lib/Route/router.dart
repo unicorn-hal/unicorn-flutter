c82b1607fb7fd54b943515fc4240fe30d4fcf97b
ishibashikeita <ishibashi.hnut@gmail.com>
2024-10-24 13:09:51 +0900

    fix: routesファイルの修正

--- lib/Route/router.dart
+++ lib/Route/router.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:unicorn_flutter/Route/navigation_shell.dart';
 import 'package:unicorn_flutter/View/Chat/chat_top_view.dart';
+import 'package:unicorn_flutter/View/HealthCheckup/Checkup/ai_checkup_view.dart';
 import 'package:unicorn_flutter/View/HealthCheckup/health_checkup_top_view.dart';
 import 'package:unicorn_flutter/View/Home/home_view.dart';
 import 'package:unicorn_flutter/View/Profile/FamilyEmail/family_email_edit_view.dart';
@@ -45,6 +46,9 @@ final routerProvider = Provider(
       routes: [
         TypedGoRoute<HealthCheckupRoute>(
           path: Routes.healthCheckup,
+        ),
+        TypedGoRoute<AiCheckupRoute>(
+          path: Routes.aiCheckup,
         ),
       ],
     ),
@@ -153,6 +157,14 @@ class HealthCheckupRoute extends GoRouteData {
   @override
   Widget build(BuildContext context, GoRouterState state) =>
       const HealthCheckupTopView();
+}
+
+class AiCheckupRoute extends GoRouteData {
+  const AiCheckupRoute();
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) =>
+      const AiCheckupView();
 }
 //////////////////////////////  healthCheckup  //////////////////////////////
 
