87dfa0cae99d14f0cf5090ed75274b6b8a0e5204
ishibashikeita <ishibashi.hnut@gmail.com>
2024-10-24 13:05:36 +0900

    add: AI検診画面追加

---
+++ lib/View/HealthCheckup/Checkup/ai_checkup_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:unicorn_flutter/View/Component/CustomWidget/custom_scaffold.dart';
+
+class AiCheckupView extends StatelessWidget {
+  const AiCheckupView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
