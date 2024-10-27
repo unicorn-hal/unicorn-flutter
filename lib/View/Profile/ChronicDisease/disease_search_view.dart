8114503bb976a3c38a2b5b9e65ee03ccea5127a2
LAPTOP-537VN1FQ\rei20 <nakada0404rei@gmail.com>
2024-10-28 03:12:04 +0900

    fix: 不要なimport削除

--- lib/View/Profile/ChronicDisease/disease_search_view.dart
+++ lib/View/Profile/ChronicDisease/disease_search_view.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_scaffold.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_text.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_textfield.dart';
