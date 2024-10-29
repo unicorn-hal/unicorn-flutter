78c9c302a13984db76c82be0839a57c929cd7f4f
ryo hasegawa <minecraftnolisio@gmail.com>
2024-10-29 14:08:03 +0900

    fix: Model 命名修正

--- lib/Model/Entity/PrimaryDoctor/primary_doctors_request.dart
+++ lib/Model/Entity/PrimaryDoctor/primary_doctors_request.dart
@@ -7,13 +7,13 @@ class PrimaryDoctorsRequest {
 
   factory PrimaryDoctorsRequest.fromJson(Map<String, dynamic> json) {
     return PrimaryDoctorsRequest(
-      doctorIds: (json['doctorIds'] as List).map((e) => e.toString()).toList(),
+      doctorIds: (json['doctorIDs'] as List).map((e) => e.toString()).toList(),
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
-      'doctorIds': doctorIds,
+      'doctorIDs': doctorIds,
     };
   }
 }
