711be474a65f0a9010c1f0bce0deca6d6f9d4fab
ishibashikeita <109339217+ishibashikeita@users.noreply.github.com>
2024-10-29 16:23:33 +0900

    fix: フォーカスの追加

--- lib/View/Chat/DoctorSearch/doctor_search_view.dart
+++ lib/View/Chat/DoctorSearch/doctor_search_view.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_appbar.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_dropdown.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_scaffold.dart';
@@ -21,6 +23,8 @@ class _DoctorSearchViewState extends State<DoctorSearchView> {
   final TextEditingController departmentNameController =
       TextEditingController();
   final TextEditingController doctorNameController = TextEditingController();
+
+  final focusNode = FocusNode();
 
   // 仮で医師リストをStringで作成
   final List<String> doctors = [
@@ -56,44 +60,46 @@ class _DoctorSearchViewState extends State<DoctorSearchView> {
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
 
-    return CustomScaffold(
-      isScrollable: true,
-      appBar: CustomAppBar(
-        title: '医師を探す',
-        foregroundColor: Colors.white,
-        backgroundColor: ColorName.mainColor,
-      ),
-      body: Column(
-        children: [
-          /// 検索内容入力部
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 4.0,
-              horizontal: 16.0,
-            ),
-            child: SizedBox(
-              width: size.width,
-              height: 54,
-              child: Row(
-                children: [
-                  const Expanded(
-                    flex: 1,
-                    child: Center(
-                      child: Padding(
-                        padding: EdgeInsets.only(left: 16.0),
-                        child: CustomText(
-                          text: '病院名 :',
+    return Focus(
+      focusNode: focusNode,
+      child: GestureDetector(
+        onTap: focusNode.requestFocus,
+        child: CustomScaffold(
+          isScrollable: true,
+          appBar: CustomAppBar(
+            title: '医師を探す',
+            foregroundColor: Colors.white,
+            backgroundColor: ColorName.mainColor,
+          ),
+          body: Column(
+            children: [
+              const SizedBox(
+                height: 4.0,
+              ),
+
+              /// 検索内容入力部
+              SizedBox(
+                width: size.width,
+                height: 54,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: size.width * 0.25,
+                      child: const Center(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 16.0),
+                          child: CustomText(
+                            text: '病院名 :',
+                          ),
                         ),
                       ),
                     ),
-                  ),
-                  Expanded(
-                    flex: 3,
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 8.0, vertical: 4.0),
+                    SizedBox(
+                      width: size.width * 0.75,
+                      child: Center(
                         child: CustomTextfield(
+                          width: size.width * 0.7,
                           height: 44,
                           hintText: '病院名を入力してください',
                           controller: hospitalNameController,
@@ -102,162 +108,148 @@ class _DoctorSearchViewState extends State<DoctorSearchView> {
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 4.0,
-              horizontal: 16.0,
-            ),
-            child: SizedBox(
-              width: size.width,
-              height: 54,
-              child: Row(
-                children: [
-                  const Expanded(
-                    flex: 1,
-                    child: Center(
-                      child: Padding(
-                        padding: EdgeInsets.only(left: 16.0),
-                        child: CustomText(
-                          text: '診療科 :',
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 8.0, vertical: 4.0),
-                        child: CustomDropdown(
-                          height: 44,
-                          dropdownItems: [
-                            // todo: controllerからforの個数決める
-                            for (String department in departments)
-                              DropdownMenuItem(
-                                value: departments.indexOf(department),
-                                child: CustomText(
-                                  text: department,
-                                  fontSize: 12,
-                                ),
-                              ),
-                          ],
-                          onChanged: (int? value) {
-                            setState(() {
-                              selectedItem = value;
-                            });
-                          },
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Container(),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 4.0,
-              horizontal: 16.0,
-            ),
-            child: SizedBox(
-              width: size.width,
-              height: 54,
-              child: Row(
-                children: [
-                  const Expanded(
-                    flex: 1,
-                    child: Center(
-                      child: Padding(
-                        padding: EdgeInsets.only(left: 16.0),
-                        child: CustomText(
-                          text: '医師名 :',
-                        ),
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 3,
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 8.0, vertical: 4.0),
-                        child: CustomTextfield(
-                          height: 44,
-                          hintText: '名前を入力してください',
-                          controller: doctorNameController,
-                          maxLength: 10,
-                          maxLines: 1,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-
-          const SpacerAndDivider(
-            topHeight: 4.0,
-            bottomHeight: 0,
-          ),
-
-          ///検索結果表示部
-          doctors.isEmpty
-              ?
-              // 医師リストが空の場合は該当する医師が見つかりませんでしたを表示
-              SizedBox(
-                  width: size.width * 0.9,
-                  height: 400,
-                  child: const Center(
-                    child: CustomText(
-                      text: '該当する医師が見つかりませんでした',
-                    ),
-                  ),
-                )
-              :
-              // 医師リストがある場合は医師リストを表示
-              Column(
-                  children: [
-                    const Align(
-                      alignment: Alignment.centerLeft,
-                      child: Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: CustomText(text: '検索結果'),
-                      ),
-                    ),
-                    ListView.builder(
-                        itemCount: doctors.length,
-                        shrinkWrap: true,
-                        physics: const NeverScrollableScrollPhysics(),
-                        itemBuilder: (context, index) {
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 4.0,
-                              vertical: 1.0,
-                            ),
-                            child: UserInfoTile(
-                              tileColor: ColorName.userInfoTileBackground,
-                              onTap: () {
-                                // todo: 医師詳細画面へ遷移
-                              },
-                              userName: doctors[index],
-                              description: '診療科: 内科',
-                            ),
-                          );
-                        }),
                   ],
                 ),
-        ],
+              ),
+              SizedBox(
+                width: size.width,
+                height: 54,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: size.width * 0.25,
+                      child: const Center(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 16.0),
+                          child: CustomText(
+                            text: '診療科 :',
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: size.width * 0.5,
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 0, vertical: 4.0),
+                          child: CustomDropdown(
+                            height: 44,
+                            dropdownItems: [
+                              // todo: controllerからforの個数決める
+                              for (String department in departments)
+                                DropdownMenuItem(
+                                  value: departments.indexOf(department),
+                                  child: CustomText(
+                                    text: department,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                            ],
+                            onChanged: (int? value) {
+                              setState(() {
+                                selectedItem = value;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                width: size.width,
+                height: 54,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: size.width * 0.25,
+                      child: const Center(
+                        child: Padding(
+                          padding: EdgeInsets.only(left: 16.0),
+                          child: CustomText(
+                            text: '医師名 :',
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: size.width * 0.75,
+                      child: Center(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 4.0),
+                          child: CustomTextfield(
+                            width: size.width * 0.6,
+                            height: 44,
+                            hintText: '名前を入力してください',
+                            controller: doctorNameController,
+                            maxLength: 10,
+                            maxLines: 1,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              const SpacerAndDivider(
+                topHeight: 4.0,
+                bottomHeight: 0,
+              ),
+
+              ///検索結果表示部
+              doctors.isEmpty
+                  ?
+                  // 医師リストが空の場合は該当する医師が見つかりませんでしたを表示
+                  SizedBox(
+                      width: size.width * 0.9,
+                      height: 400,
+                      child: const Center(
+                        child: CustomText(
+                          text: '該当する医師が見つかりませんでした',
+                        ),
+                      ),
+                    )
+                  :
+                  // 医師リストがある場合は医師リストを表示
+                  Column(
+                      children: [
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: CustomText(text: '検索結果'),
+                          ),
+                        ),
+                        ListView.builder(
+                            itemCount: doctors.length,
+                            shrinkWrap: true,
+                            physics: const NeverScrollableScrollPhysics(),
+                            itemBuilder: (context, index) {
+                              return Padding(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 4.0,
+                                  vertical: 1.0,
+                                ),
+                                child: UserInfoTile(
+                                  tileColor: ColorName.userInfoTileBackground,
+                                  onTap: () {
+                                    // todo: 医師詳細画面へ遷移
+                                  },
+                                  userName: doctors[index],
+                                  description: '診療科: 内科',
+                                ),
+                              );
+                            }),
+                      ],
+                    ),
+            ],
+          ),
+        ),
       ),
     );
   }
