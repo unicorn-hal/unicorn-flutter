c507433766849359717d5898dd7233614fd2f0a5
school\rei20 <nakada0404rei@gmail.com>
2024-10-29 14:04:59 +0900

    fix: 不要なSizedBox削除

--- lib/View/Component/CustomWidget/custom_textfield.dart
+++ lib/View/Component/CustomWidget/custom_textfield.dart
@@ -43,133 +43,130 @@ class CustomTextfield extends StatefulWidget {
 class _CustomTextfieldState extends State<CustomTextfield> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: widget.width,
-      child: Row(
-        children: [
-          SizedBox(
-            width: widget.useSearchButton ? widget.width * 0.85 : widget.width,
-            child: TextFormField(
-              keyboardType: widget.keyboardType,
-              inputFormatters: widget.inputFormatters,
-              controller: widget.controller,
-              onChanged: (value) {
-                setState(() {});
-              },
-              textAlignVertical: TextAlignVertical.center,
-              style: const TextStyle(
+    return Row(
+      children: [
+        SizedBox(
+          width: widget.useSearchButton ? widget.width * 0.85 : widget.width,
+          child: TextFormField(
+            keyboardType: widget.keyboardType,
+            inputFormatters: widget.inputFormatters,
+            controller: widget.controller,
+            onChanged: (value) {
+              setState(() {});
+            },
+            textAlignVertical: TextAlignVertical.center,
+            style: const TextStyle(
+              fontSize: 16,
+              height: 1.5,
+              decorationColor: ColorName.mainColor,
+            ),
+            decoration: InputDecoration(
+              counterText: '',
+              prefixIcon: widget.prefixIcon,
+              suffixIcon: Visibility(
+                visible: widget.controller.text.isNotEmpty,
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.cancel_rounded,
+                    color: Colors.grey,
+                  ),
+                  onPressed: () {
+                    widget.controller.text = '';
+                    setState(() {});
+                  },
+                ),
+              ),
+              border: InputBorder.none,
+              hintText: widget.hintText,
+              hintStyle: const TextStyle(
+                decoration: TextDecoration.none,
+                color: Colors.grey,
                 fontSize: 16,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Noto_Sans_JP',
                 height: 1.5,
-                decorationColor: ColorName.mainColor,
               ),
-              decoration: InputDecoration(
-                counterText: '',
-                prefixIcon: widget.prefixIcon,
-                suffixIcon: Visibility(
-                  visible: widget.controller.text.isNotEmpty,
-                  child: IconButton(
-                    icon: const Icon(
-                      Icons.cancel_rounded,
-                      color: Colors.grey,
-                    ),
-                    onPressed: () {
-                      widget.controller.text = '';
-                      setState(() {});
-                    },
-                  ),
-                ),
-                border: InputBorder.none,
-                hintText: widget.hintText,
-                hintStyle: const TextStyle(
-                  decoration: TextDecoration.none,
+              filled: true,
+              fillColor: widget.backgroundcolor,
+              enabledBorder: OutlineInputBorder(
+                borderRadius: widget.useSearchButton
+                    ? const BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        bottomLeft: Radius.circular(30),
+                      )
+                    : BorderRadius.circular(30),
+                borderSide: const BorderSide(
+                  width: 1,
                   color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Noto_Sans_JP',
-                  height: 1.5,
                 ),
-                filled: true,
-                fillColor: widget.backgroundcolor,
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: widget.useSearchButton
-                      ? const BorderRadius.only(
-                          topLeft: Radius.circular(30),
-                          bottomLeft: Radius.circular(30),
-                        )
-                      : BorderRadius.circular(30),
-                  borderSide: const BorderSide(
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: widget.useSearchButton
+                    ? const BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        bottomLeft: Radius.circular(30),
+                      )
+                    : BorderRadius.circular(30),
+                borderSide: const BorderSide(
+                  width: 1,
+                  color: Colors.grey,
+                ),
+              ),
+              isDense: true,
+              contentPadding: EdgeInsets.symmetric(
+                vertical: (widget.height - 20) / 2,
+                // (widget.height - (TextStyle(fontSize) * TextStyle(height))) / 2, + (textPadding or TextFormFieldMargin)
+                horizontal: widget.prefixIcon != null ? 5 : 20,
+              ),
+            ),
+            cursorColor: ColorName.mainColor,
+            cursorWidth: 2,
+            cursorRadius: const Radius.circular(10),
+            maxLines: widget.useSearchButton ? 1 : widget.maxLines,
+            minLines: 1,
+            maxLength: widget.maxLength,
+          ),
+        ),
+        Visibility(
+          visible: widget.useSearchButton,
+          child: GestureDetector(
+            onTap: () {
+              widget.buttonOnTap!();
+            },
+            child: Container(
+              height: widget.height + 4,
+              // (widget.height + 4 - 24) + (24 * 行数)
+              // 行数が変動したタイミングとそのときの行数を持ってこれたら上の式に代入
+              width: widget.width * 0.15,
+              decoration: const BoxDecoration(
+                color: ColorName.mainColor,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30),
+                  bottomRight: Radius.circular(30),
+                ),
+                border: Border(
+                  top: BorderSide(
+                    width: 1,
+                    color: Colors.grey,
+                  ),
+                  right: BorderSide(
+                    width: 1,
+                    color: Colors.grey,
+                  ),
+                  bottom: BorderSide(
                     width: 1,
                     color: Colors.grey,
                   ),
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: widget.useSearchButton
-                      ? const BorderRadius.only(
-                          topLeft: Radius.circular(30),
-                          bottomLeft: Radius.circular(30),
-                        )
-                      : BorderRadius.circular(30),
-                  borderSide: const BorderSide(
-                    width: 1,
-                    color: Colors.grey,
-                  ),
-                ),
-                isDense: true,
-                contentPadding: EdgeInsets.symmetric(
-                  vertical: (widget.height - 20) / 2,
-                  // (widget.height - (TextStyle(fontSize) * TextStyle(height))) / 2, + (textPadding or TextFormFieldMargin)
-                  horizontal: widget.prefixIcon != null ? 5 : 20,
-                ),
               ),
-              cursorColor: ColorName.mainColor,
-              cursorWidth: 2,
-              cursorRadius: const Radius.circular(10),
-              maxLines: widget.useSearchButton ? 1 : widget.maxLines,
-              minLines: 1,
-              maxLength: widget.maxLength,
-            ),
-          ),
-          Visibility(
-            visible: widget.useSearchButton,
-            child: GestureDetector(
-              onTap: () {
-                widget.buttonOnTap!();
-              },
-              child: Container(
-                height: widget.height + 4,
-                // (widget.height + 4 - 24) + (24 * 行数)
-                // 行数が変動したタイミングとそのときの行数を持ってこれたら上の式に代入
-                width: widget.width * 0.15,
-                decoration: const BoxDecoration(
-                  color: ColorName.mainColor,
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(30),
-                    bottomRight: Radius.circular(30),
-                  ),
-                  border: Border(
-                    top: BorderSide(
-                      width: 1,
-                      color: Colors.grey,
-                    ),
-                    right: BorderSide(
-                      width: 1,
-                      color: Colors.grey,
-                    ),
-                    bottom: BorderSide(
-                      width: 1,
-                      color: Colors.grey,
-                    ),
-                  ),
-                ),
-                child: const Icon(
-                  Icons.search,
-                  color: Colors.white,
-                ),
+              child: const Icon(
+                Icons.search,
+                color: Colors.white,
               ),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
