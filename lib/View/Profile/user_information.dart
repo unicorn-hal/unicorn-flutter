86573117e8644df89efc5a84b69c70559e67f4e2
ryudra131 <ryunosuke.13043702@gmail.com>
2024-10-30 23:37:03 +0900

    add: UserInformationViewの追加

---
+++ lib/View/Profile/user_information.dart
@@ -0,0 +1,182 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:unicorn_flutter/View/Component/CustomWidget/custom_scaffold.dart';
+import 'package:unicorn_flutter/View/Component/CustomWidget/custom_text.dart';
+import 'package:unicorn_flutter/View/Component/CustomWidget/custom_textfield.dart';
+import 'package:unicorn_flutter/View/Component/Parts/circle_button.dart';
+import 'package:unicorn_flutter/View/Component/Parts/user_image_circle.dart';
+import 'package:unicorn_flutter/gen/colors.gen.dart';
+
+class UserInfomationView extends StatefulWidget {
+  const UserInfomationView({super.key});
+
+  @override
+  State<UserInfomationView> createState() => _UserInfomationViewState();
+}
+
+class _UserInfomationViewState extends State<UserInfomationView> {
+  final TextEditingController phoneNumber = TextEditingController();
+  final TextEditingController mailAddress = TextEditingController();
+  final TextEditingController occupation = TextEditingController();
+  final ImagePicker picker = ImagePicker();
+  final FocusNode focusnode = FocusNode();
+
+  // todo: Controllerが完成次第、ここに追記または変更していきます。
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceWidth = MediaQuery.of(context).size.width;
+    return CustomScaffold(
+      focusNode: focusnode,
+      isAppbar: false,
+      isScrollable: true,
+      body: SafeArea(
+        child: SizedBox(
+          width: deviceWidth,
+          child: FractionallySizedBox(
+            widthFactor: 0.85,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(color: ColorName.textGray),
+                    ),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.only(bottom: 10),
+                    child: Align(
+                        alignment: Alignment.topCenter,
+                        child: Column(
+                          children: [
+                            CustomText(
+                              text: 'User',
+                              color: ColorName.profileInputBackgroundColor,
+                              fontSize: 22,
+                            ),
+                            CustomText(
+                              text: 'ユーザー情報を入力してください',
+                              color: ColorName.textBlack,
+                              fontSize: 22,
+                            ),
+                          ],
+                        )),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 30),
+                  child: CustomText(
+                    text: 'プロフィール画像',
+                    fontSize: 20,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40, bottom: 40),
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: SizedBox(
+                      width: 200,
+                      height: 200,
+                      child: Stack(
+                        children: [
+                          UserImageCircle(
+                            imageSize: 200,
+                            localImage: Image.asset(
+                                "assets/images/icons/default_image_icon.png"),
+                            imageUrl: '',
+                          ),
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: CircleButton(
+                              buttonSize: 50,
+                              buttonColor: Colors.white,
+                              borderColor:
+                                  ColorName.imageSelectCirclebuttonColor,
+                              onTap: () {},
+                              icon: const Icon(Icons.edit_outlined),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: CustomText(
+                    text: '電話番号',
+                    fontSize: 20,
+                  ),
+                ),
+                CustomTextfield(
+                  hintText: '0120-999-9999',
+                  width: deviceWidth * 0.85,
+                  height: 44,
+                  keyboardType: TextInputType.phone,
+                  controller: phoneNumber,
+                  maxLines: 1,
+                  maxLength: 14,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 20, bottom: 10),
+                  child: CustomText(
+                    text: 'メールアドレス',
+                    fontSize: 20,
+                  ),
+                ),
+                CustomTextfield(
+                  hintText: 'hogehoge@gmail.com',
+                  width: deviceWidth * 0.85,
+                  height: 44,
+                  controller: mailAddress,
+                  maxLines: 1,
+                  maxLength: 30,
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 20, bottom: 10),
+                  child: CustomText(
+                    text: '職業',
+                    fontSize: 20,
+                  ),
+                ),
+                CustomTextfield(
+                  hintText: '会社員、主婦、学生など',
+                  width: deviceWidth * 0.85,
+                  height: 44,
+                  controller: occupation,
+                  maxLines: 1,
+                  maxLength: 15,
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  // todo: 次のViewができ次第ルーティングします。
+                  child: Align(
+                    alignment: Alignment.center,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 30, bottom: 30),
+                      child: Container(
+                          width: deviceWidth * 0.5,
+                          height: 60,
+                          color: ColorName.profileInputButtonColor,
+                          child: const Center(
+                            child: CustomText(
+                              text: '次に進む',
+                              fontSize: 22,
+                              color: Colors.white,
+                            ),
+                          )),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
