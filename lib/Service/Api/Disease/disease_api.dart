45873c35c007447957ddd616099298e4d6b3f398
ryo hasegawa <minecraftnolisio@gmail.com>
2024-10-31 11:41:41 +0900

    add: disease_api 追記

--- lib/Service/Api/Disease/disease_api.dart
+++ lib/Service/Api/Disease/disease_api.dart
@@ -1,6 +1,38 @@
+import 'package:unicorn_flutter/Model/Entity/Disease/disease.dart';
 import 'package:unicorn_flutter/Service/Api/Core/api_core.dart';
 import 'package:unicorn_flutter/Service/Api/Core/endpoint.dart';
 
 class DiseaseApi extends ApiCore with Endpoint {
   DiseaseApi() : super(Endpoint.diseases);
+
+  /// GET
+  /// 病名から病気一覧を取得
+  /// [diseaseName] 病名
+  Future<List<Disease>?> getDiseaseList({
+    required String diseaseName,
+  }) async {
+    try {
+      useParameter(parameter: '?diseaseName=$diseaseName');
+      final response = await get();
+      return (response.data['data'] as List)
+          .map((e) => Disease.fromJson(e))
+          .toList();
+    } catch (e) {
+      return null;
+    }
+  }
+
+  /// GET
+  /// 有名な病気一覧を取得
+  Future<List<Disease>?> getFamousDiseaseList() async {
+    useParameter(parameter: '/famous');
+    try {
+      final response = await get();
+      return (response.data['data'] as List)
+          .map((e) => Disease.fromJson(e))
+          .toList();
+    } catch (e) {
+      return null;
+    }
+  }
 }
