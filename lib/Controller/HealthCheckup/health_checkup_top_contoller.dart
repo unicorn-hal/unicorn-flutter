d9713b297bde78416d033c39e1af41dadcd0decc
ishibashikeita <109339217+ishibashikeita@users.noreply.github.com>
2024-11-06 07:18:03 +0900

    fix: 雛形作成

--- lib/Controller/HealthCheckup/health_checkup_top_contoller.dart
+++ lib/Controller/HealthCheckup/health_checkup_top_contoller.dart
@@ -1,1 +1,27 @@
-class HealthCheckupTopController {}
+import 'package:unicorn_flutter/Controller/Core/controller_core.dart';
+import 'package:unicorn_flutter/Model/Entity/HealthCheckUp/health_checkup.dart';
+import 'package:unicorn_flutter/Service/Api/HealthCheckup/health_checkup_api.dart';
+
+class HealthCheckupTopController extends ControllerCore {
+  // Serviceのインスタンス化
+  HealthCheckupApi get _healthCheckupApi => HealthCheckupApi();
+
+  // コンストラクタ
+  HealthCheckupTopController();
+
+  // 変数の定義
+  bool? alreadyCheackup;
+
+  // initialize()
+  @override
+  void initialize() {}
+
+  // 各関数の定義
+
+  // 健康診断結果一覧取得
+  Future<List<HealthCheckup>> getHealthCheckupList() async {
+    final List<HealthCheckup> healthCheckupList =
+        await _healthCheckupApi.getHealthCheckupList() ?? [];
+    return healthCheckupList;
+  }
+}
