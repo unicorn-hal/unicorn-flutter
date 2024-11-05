b2643401b8abbec02cd73d0aedf77785a1ff9b53
kakeru-ikeda <kakerumail1451@gmail.com>
2024-11-05 21:06:01 +0900

    add: TopLoadingView 仮完成

--- lib/View/top_loading_view.dart
+++ lib/View/top_loading_view.dart
@@ -1,33 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:unicorn_flutter/Controller/top_loading_controller.dart';
 import 'package:unicorn_flutter/Service/Log/log_service.dart';
+import 'package:unicorn_flutter/View/Component/CustomWidget/custom_scaffold.dart';
+import 'package:unicorn_flutter/View/Component/CustomWidget/custom_text.dart';
+import 'package:unicorn_flutter/gen/assets.gen.dart';
+import 'package:unicorn_flutter/gen/colors.gen.dart';
 
 class TopLoadingView extends StatelessWidget {
   const TopLoadingView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    late TopLoadingController controller;
     try {
-      TopLoadingController(context);
+      controller = TopLoadingController(context);
     } catch (e) {
       Log.echo('TopLoading Error: $e', symbol: '❌');
     }
     final double deviceHeight = MediaQuery.of(context).size.height;
 
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const CircularProgressIndicator(),
-            const SizedBox(height: 16),
-            Text(
-              'Now Loading...',
-              style: TextStyle(
-                fontSize: deviceHeight * 0.03,
+    return CustomScaffold(
+      isAppbar: false,
+      body: Container(
+        height: deviceHeight,
+        color: ColorName.topLoadingBackground,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Assets.images.topLoading.image(),
+              const SizedBox(height: 16),
+              LoadingAnimationWidget.fourRotatingDots(
+                color: Colors.amber,
+                size: 54,
               ),
-            ),
-          ],
+              const SizedBox(height: 16),
+              const CustomText(
+                text: 'Loading ...',
+                fontWeight: FontWeight.w500,
+              ),
+              const SizedBox(height: 48),
+              FutureBuilder(
+                future: controller.appVersion,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return CustomText(
+                      text: 'v${snapshot.data}',
+                      color: ColorName.textGray,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 12,
+                    );
+                  } else {
+                    return const SizedBox();
+                  }
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
