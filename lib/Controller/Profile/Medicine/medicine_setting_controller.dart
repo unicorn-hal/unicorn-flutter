64786635f0b2a6d0f53a2463afdd0309b4017cea
school\rei20 <nakada0404rei@gmail.com>
2024-11-05 17:39:29 +0900

    add: MedicineSettingController作成

---
+++ lib/Controller/Profile/Medicine/medicine_setting_controller.dart
@@ -0,0 +1,18 @@
+import 'package:unicorn_flutter/Controller/Core/controller_core.dart';
+
+class MedicineSettingController extends ControllerCore {
+  /// Serviceのインスタンス化
+
+  /// コンストラクタ
+  MedicineSettingController();
+
+  /// 変数の定義
+
+  /// initialize()
+  @override
+  void initialize() {
+    print('Controller Init');
+  }
+
+  /// 各関数の実装
+}
