91fe04838c6bfaf55e6a861998ff7f95d7da8abc
ishibashikeita <ishibashi.hnut@gmail.com>
2024-11-07 20:06:56 +0900

    fix: コントローラーの初期化

--- lib/Controller/Chat/chat_top_controller.dart
+++ lib/Controller/Chat/chat_top_controller.dart
@@ -1,1 +1,11 @@
-class ChatTopController {}
+import 'package:unicorn_flutter/Controller/Core/controller_core.dart';
+import 'package:unicorn_flutter/Service/Api/Chat/chat_api.dart';
+
+class ChatTopController extends ControllerCore {
+  ChatApi get _chatApi => ChatApi();
+
+  ChatTopController();
+
+  @override
+  void initialize() {}
+}
