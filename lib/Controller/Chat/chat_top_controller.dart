e30195ce95aaaaf89eef008fb65b2ce5990b885b
ishibashikeita <ishibashi.hnut@gmail.com>
2024-11-07 20:54:10 +0900

    fix: レビュー用修正

--- lib/Controller/Chat/chat_top_controller.dart
+++ lib/Controller/Chat/chat_top_controller.dart
@@ -1,9 +1,6 @@
 import 'package:unicorn_flutter/Controller/Core/controller_core.dart';
-import 'package:unicorn_flutter/Service/Api/Chat/chat_api.dart';
 
 class ChatTopController extends ControllerCore {
-  ChatApi get _chatApi => ChatApi();
-
   ChatTopController();
 
   @override
