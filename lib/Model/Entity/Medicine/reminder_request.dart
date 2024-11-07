29304fce468912de0df2d2eda1edbd34f6900699
kakeru-ikeda <kakerumail1451@gmail.com>
2024-11-07 22:14:17 +0900

    fix: ReminderRequestにReminderIDを登録できるように変更

--- lib/Model/Entity/Medicine/reminder_request.dart
+++ lib/Model/Entity/Medicine/reminder_request.dart
@@ -1,16 +1,19 @@
 import 'package:unicorn_flutter/Constants/Enum/day_of_week_enum.dart';
 
 class ReminderRequest {
+  final String reminderId;
   final String reminderTime;
   final List<DayOfWeekEnum> reminderDayOfWeek;
 
   ReminderRequest({
+    required this.reminderId,
     required this.reminderTime,
     required this.reminderDayOfWeek,
   });
 
   factory ReminderRequest.fromJson(Map<String, dynamic> json) {
     return ReminderRequest(
+      reminderId: json['reminderID'],
       reminderTime: json['reminderTime'],
       reminderDayOfWeek: (json['reminderDayOfWeek'] as List)
           .map((e) => DayOfWeekEnumType.fromString(e))
@@ -20,6 +23,7 @@ class ReminderRequest {
 
   Map<String, dynamic> toJson() {
     return {
+      'reminderID': reminderId,
       'reminderTime': reminderTime,
       'reminderDayOfWeek': reminderDayOfWeek
           .map((e) => DayOfWeekEnumType.toStringValue(e))
