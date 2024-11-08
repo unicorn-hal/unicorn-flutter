609bc0751668d95cb1436bdce6b67a7eeb4474ad
ishibashikeita <ishibashi.hnut@gmail.com>
2024-11-08 17:49:24 +0900

    fix: 検索結果をStreamで検知

--- lib/Controller/Chat/Doctor/doctor_search_controller.dart
+++ lib/Controller/Chat/Doctor/doctor_search_controller.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:unicorn_flutter/Model/Data/Department/department_data.dart';
 import 'package:unicorn_flutter/Service/Api/Doctor/doctor_api.dart';
@@ -14,41 +16,71 @@ class DoctorSearchController extends ControllerCore {
   final TextEditingController _hospitalNameController = TextEditingController();
   final TextEditingController _doctorNameController = TextEditingController();
 
+  final StreamController<List<Doctor>> _doctorListController =
+      StreamController.broadcast();
+
+  // 1秒入力されなかったら検索を実行するためのタイマー
+  Timer? _debounce;
+
   late int _selectedDepartmentIndex;
   late List<Department> _departmentList;
-  late List<Doctor> _doctorList;
+  List<Doctor> _doctorList = [];
 
   @override
   void initialize() {
     _departmentList = DepartmentData().data;
     _selectedDepartmentIndex = 0;
-    _doctorList = [];
 
-    print(_doctorList);
+    // 1秒入力されなかったら検索を実行するリスナーを設定
+    _hospitalNameController.addListener(() {
+      _onSearchChanged();
+    });
+    _doctorNameController.addListener(() {
+      _onSearchChanged();
+    });
+
+    //StreamControllerに初期値を流す
+    _doctorListController.sink.add(_doctorList);
   }
 
+  // 医師一覧のgetter
   List<Department> get departmentList => _departmentList;
 
+  // テキストコントローラーのgetter
   TextEditingController get hospitalNameController => _hospitalNameController;
   TextEditingController get doctorNameController => _doctorNameController;
 
-  void setSelectedDepartmentIndex(int index) {
+  // 1秒待ってから検索を実行する
+  void _onSearchChanged() {
+    if (_debounce?.isActive ?? false) _debounce!.cancel();
+    _debounce = Timer(const Duration(seconds: 1), () async {
+      await _searchDoctors();
+    });
+  }
+
+  // 選択された科を設定したときに検索を実行する
+  void setSelectedDepartmentIndex(int index) async {
     _selectedDepartmentIndex = index;
+    _onSearchChanged();
   }
 
   int get selectedDepartmentIndex => _selectedDepartmentIndex;
 
-  Future<void> searchDoctors() async {
+  // 設定されたクエリで医師検索を実行する
+  Future<void> _searchDoctors() async {
     final String hospitalName = _hospitalNameController.text;
     final String doctorName = _doctorNameController.text;
     _doctorList = await _doctorApi.getDoctorList(
           doctorName: doctorName,
           hospitalName: hospitalName,
           departmentId:
-              DepartmentData().data[_selectedDepartmentIndex].departmentName,
+              DepartmentData().data[_selectedDepartmentIndex].departmentId,
         ) ??
         [];
+
+    _doctorListController.sink.add(_doctorList);
   }
 
-  List<Doctor> get doctorList => _doctorList;
+  // 医師一覧のStream/getter
+  Stream<List<Doctor>> get doctorListStream => _doctorListController.stream;
 }
--- lib/View/Chat/Doctor/Search/doctor_search_view.dart
+++ lib/View/Chat/Doctor/Search/doctor_search_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:unicorn_flutter/Controller/Chat/Doctor/doctor_search_controller.dart';
 import 'package:unicorn_flutter/Model/Entity/Department/department.dart';
+import 'package:unicorn_flutter/Model/Entity/Doctor/doctor.dart';
+import 'package:unicorn_flutter/Route/router.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_appbar.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_dropdown.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_scaffold.dart';
@@ -11,7 +13,7 @@ import 'package:unicorn_flutter/View/Component/Parts/user_info_tile.dart';
 import 'package:unicorn_flutter/gen/colors.gen.dart';
 
 class DoctorSearchView extends StatefulWidget {
-  DoctorSearchView({super.key});
+  const DoctorSearchView({super.key});
 
   @override
   State<DoctorSearchView> createState() => _DoctorSearchViewState();
@@ -20,13 +22,21 @@ class DoctorSearchView extends StatefulWidget {
 class _DoctorSearchViewState extends State<DoctorSearchView> {
   late DoctorSearchController controller;
 
-  final focusNode = FocusNode();
-
   @override
   void initState() {
     super.initState();
     controller = DoctorSearchController();
   }
+
+  @override
+  void dispose() {
+    super.dispose();
+    // Viewが破棄される際にControllerにあるリスナーをdisposeする
+    controller.doctorNameController.dispose();
+    controller.hospitalNameController.dispose();
+  }
+
+  final focusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
@@ -119,7 +129,6 @@ class _DoctorSearchViewState extends State<DoctorSearchView> {
                         ],
                         onChanged: (int? value) {
                           controller.setSelectedDepartmentIndex(value!);
-                          setState(() {});
                         },
                       ),
                     ),
@@ -171,52 +180,56 @@ class _DoctorSearchViewState extends State<DoctorSearchView> {
           ),
 
           ///検索結果表示部
-          if (controller.doctorList.isEmpty)
-            // 医師リストが空の場合は該当する医師が見つかりませんでしたを表示
-            SizedBox(
-              width: size.width * 0.9,
-              height: 400,
-              child: const Center(
-                child: CustomText(
-                  text: '該当する医師が見つかりませんでした',
+          Column(
+            children: [
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: CustomText(text: '検索結果'),
                 ),
               ),
-            )
-          else
-            // 医師リストがある場合は医師リストを表示
-            Column(
-              children: [
-                const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: CustomText(text: '検索結果'),
-                  ),
-                ),
-                ListView.builder(
-                    itemCount: controller.doctorList.length,
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(
-                          horizontal: 4.0,
-                          vertical: 1.0,
-                        ),
-                        child: UserInfoTile(
-                          tileColor: ColorName.userInfoTileBackground,
-                          onTap: () {
-                            // todo: 医師詳細画面へ遷移
-                          },
-                          userName: controller.doctorList[index].firstName +
-                              controller.doctorList[index].lastName,
-                          description:
-                              '診療科: ${controller.doctorList[index].departments.map((e) => e.departmentName).join(',')}',
+              StreamBuilder<List<Doctor>>(
+                  stream: controller.doctorListStream,
+                  builder: (context, snapshot) {
+                    if (snapshot.data?.isEmpty ?? true) {
+                      return const Center(
+                        child: SizedBox(
+                          width: 300,
+                          height: 300,
+                          child: Center(
+                            child: CustomText(text: '該当する医師が見つかりませんでした'),
+                          ),
                         ),
                       );
-                    }),
-              ],
-            ),
+                    }
+                    return ListView.builder(
+                        itemCount: snapshot.data?.length ?? 0,
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 4.0,
+                              vertical: 1.0,
+                            ),
+                            child: UserInfoTile(
+                              tileColor: ColorName.userInfoTileBackground,
+                              onTap: () {
+                                ChatDoctorInformationRoute(
+                                        snapshot.data![index].doctorId)
+                                    .push(context);
+                              },
+                              userName: snapshot.data![index].firstName +
+                                  snapshot.data![index].lastName,
+                              description:
+                                  '病院: ${snapshot.data![index].hospital.hospitalName} 診療科: ${snapshot.data![index].departments.map((e) => e.departmentName).join(',')}',
+                            ),
+                          );
+                        });
+                  }),
+            ],
+          ),
         ],
       ),
     );
