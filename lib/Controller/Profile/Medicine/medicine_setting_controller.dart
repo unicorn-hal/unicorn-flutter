08eb2fa51bfc9534546efd924797f667ddd43eb5
LAPTOP-537VN1FQ\rei20 <nakada0404rei@gmail.com>
2024-11-09 01:25:19 +0900

    fix: reminderDayOfWeekをDayOfWeekEnumと同じ順番に並べ替え

--- lib/Controller/Profile/Medicine/medicine_setting_controller.dart
+++ lib/Controller/Profile/Medicine/medicine_setting_controller.dart
@@ -33,13 +33,13 @@ class MedicineSettingController extends ControllerCore {
   }
 
   /// 各関数の実装
-  /// Stringの時間をDateTime型に変換する関数
+  /// String型で受け取ったreminderTimeをDateTime型に変換する関数
   DateTime changeDateTime(int index) {
     return DateTime.parse(
         '${DateFormat('yyyy-MM-dd').format(DateTime.now())} ${reminders[index].reminderTime}');
   }
 
-  ///リマインダーに初期値を入れ、追加する関数
+  ///リマインダーに初期値を入れ、remindersに追加する関数
   void addReminders() {
     DateTime now = DateTime.now();
     String reminderId = const Uuid().v4();
@@ -61,12 +61,12 @@ class MedicineSettingController extends ControllerCore {
     reminders.add(reminder);
   }
 
-  ///リマインダーを削除する関数
+  ///remindersから選択されたリマインダーを削除する関数
   void deleteReminders(int index) {
     reminders.removeAt(index);
   }
 
-  ///リマインダーに登録されている時間を更新する
+  ///リマインダーに登録されている時間を更新する関数
   void updateReminderTime({required DateTime date, required int index}) {
     DateTime formatTime = DateTime(
       date.year,
@@ -77,12 +77,13 @@ class MedicineSettingController extends ControllerCore {
     );
     String reminderTime = DateFormat('HH:mm').format(formatTime);
     reminders[index] = Reminder(
-        reminderId: reminders[index].reminderId,
-        reminderTime: reminderTime,
-        reminderDayOfWeek: reminders[index].reminderDayOfWeek);
+      reminderId: reminders[index].reminderId,
+      reminderTime: reminderTime,
+      reminderDayOfWeek: reminders[index].reminderDayOfWeek,
+    );
   }
 
-  ///リマインダーの曜日listに曜日を追加する関数
+  ///reminderDayOfWeekに曜日を追加、削除する関数
   void addReminderDayOfWeek({required int remindersIndex, required int index}) {
     if (!reminders[remindersIndex]
         .reminderDayOfWeek
@@ -92,13 +93,16 @@ class MedicineSettingController extends ControllerCore {
           .add(DayOfWeekEnumType.fromWeekday(index + 1));
       return;
     }
+    if (reminders[remindersIndex].reminderDayOfWeek.length <= 1) {
+      return;
+    }
     reminders[remindersIndex]
         .reminderDayOfWeek
         .remove(DayOfWeekEnumType.fromWeekday(index + 1));
     return;
   }
 
-  ///intから漢字の曜日に変える関数
+  ///intからModalに表示する漢字表記の曜日に変える関数
   String changeWeekday(int index) {
     return DayOfWeekEnumType.toStringValueForKanji(
         DayOfWeekEnumType.fromWeekday(index + 1));
@@ -112,7 +116,7 @@ class MedicineSettingController extends ControllerCore {
         .contains(DayOfWeekEnumType.fromWeekday(index + 1));
   }
 
-  ///reminderDayOfWeekを表示する形に成形する関数
+  ///reminderDayOfWeekをMedicineSettingViewに表示する形に成形する関数
   String moldingReminderDayOfWeek(int index) {
     String displayedReminderDayOfWeek = '';
     if (reminders[index].reminderDayOfWeek.length == 7) {
@@ -121,8 +125,8 @@ class MedicineSettingController extends ControllerCore {
     if (reminders[index].reminderDayOfWeek.length == 1) {
       return '毎${DayOfWeekEnumType.toStringValueForKanji(reminders[index].reminderDayOfWeek[0])}曜日';
     }
+    reminders[index].reminderDayOfWeek.sort((a, b) => a.index - b.index);
     for (var i = 0; i < reminders[index].reminderDayOfWeek.length; i++) {
-      //ソート
       if (i == 0) {
         displayedReminderDayOfWeek =
             '$displayedReminderDayOfWeek${DayOfWeekEnumType.toStringValueForKanji(reminders[index].reminderDayOfWeek[i])}';
