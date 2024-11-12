fb844535ee7623b2c29e8611f38e0c587cad478e
LAPTOP-537VN1FQ\rei20 <nakada0404rei@gmail.com>
2024-11-13 03:40:09 +0900

    add: setState追加

--- lib/View/Profile/ChronicDisease/chronic_disease_view.dart
+++ lib/View/Profile/ChronicDisease/chronic_disease_view.dart
@@ -164,6 +164,7 @@ class _ChronicDiseaseViewState extends State<ChronicDiseaseView> {
                                             chronicDiseaseList[index]
                                                 .chronicDiseaseId);
                                         ProtectorNotifier().disableProtector();
+                                        setState(() {});
                                       },
                                     );
                                   },
