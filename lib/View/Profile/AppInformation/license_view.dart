c3730b90f8cd328085595c6450bf0230a65926d0
LAPTOP-537VN1FQ\rei20 <nakada0404rei@gmail.com>
2024-11-12 21:49:49 +0900

    add: LicenseView作成

---
+++ lib/View/Profile/AppInformation/license_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class LicenseView extends StatelessWidget {
+  const LicenseView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
