a2b01a84187e719b60e496c6cf4734a2a59748b3
ryudra131 <ryunosuke.13043702@gmail.com>
2024-11-14 17:21:00 +0900

    add: addressInfoModel追加

---
+++ lib/Model/Entity/User/address_info.dart
@@ -0,0 +1,13 @@
+class AddressInfo {
+  final int postCode;
+  final String prefectures;
+  final String municipalities;
+  final String addressDetail;
+
+  AddressInfo({
+    required this.postCode,
+    required this.prefectures,
+    required this.municipalities,
+    required this.addressDetail,
+  });
+}
