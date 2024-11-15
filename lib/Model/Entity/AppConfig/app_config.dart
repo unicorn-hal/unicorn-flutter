6a6a73dd60aff2a6b25c4bda8dc4daddec9a8a59
ryo hasegawa <minecraftnolisio@gmail.com>
2024-11-16 02:10:49 +0900

    add:app_config stunServerType 値　追加

--- lib/Model/Entity/AppConfig/app_config.dart
+++ lib/Model/Entity/AppConfig/app_config.dart
@@ -1,19 +1,23 @@
 class AppConfig {
   final bool available;
+  final String stunServerType;
 
   AppConfig({
     required this.available,
+    required this.stunServerType,
   });
 
   factory AppConfig.fromJson(Map<String, dynamic> json) {
     return AppConfig(
       available: json['available'],
+      stunServerType: json['stunServerType'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
       'available': available,
+      'stunServerType': stunServerType,
     };
   }
 }
