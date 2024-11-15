f89683b217dac97ee0d486c18c97911219cf6a55
kakeru-ikeda <kakerumail1451@gmail.com>
2024-11-15 15:46:53 +0900

    feat: ビデオ通話中Viewの改修

--- lib/View/Chat/Doctor/VoiceCall/voice_call_view.dart
+++ lib/View/Chat/Doctor/VoiceCall/voice_call_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webrtc/flutter_webrtc.dart';
 import 'package:unicorn_flutter/Controller/Chat/Doctor/VoiceCall/voice_call_controller.dart';
+import 'package:unicorn_flutter/Route/router.dart';
+import 'package:unicorn_flutter/View/Component/CustomWidget/custom_scaffold.dart';
+import 'package:unicorn_flutter/View/Component/Parts/circle_button.dart';
 
 class VoiceCallView extends StatefulWidget {
   final String calleeUid;
@@ -13,6 +16,7 @@ class VoiceCallView extends StatefulWidget {
 
 class _VoiceCallViewState extends State<VoiceCallView> {
   late VoiceCallController _controller;
+  Offset _localVideoOffset = const Offset(20, 100);
 
   @override
   void initState() {
@@ -43,14 +47,14 @@ class _VoiceCallViewState extends State<VoiceCallView> {
 
   void _onEndCall() {
     _controller.endCall();
-    Navigator.of(context).pop(); // 前の画面に戻る
+    const HomeRoute().go(context);
   }
 
   @override
   Widget build(BuildContext context) {
     if (!_controller.isCallConnected.value) {
       // 通話待機画面
-      return Scaffold(
+      return CustomScaffold(
         appBar: AppBar(
           title: Text('${widget.calleeUid} さんとの通話中...'),
         ),
@@ -60,52 +64,121 @@ class _VoiceCallViewState extends State<VoiceCallView> {
       );
     } else {
       // ビデオ通話画面
-      return Scaffold(
+      return CustomScaffold(
         appBar: AppBar(
           title: Text('ビデオ通話 - ${_controller.userId}'),
         ),
-        body: Column(
-          children: [
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: Column(
-                      children: [
-                        Expanded(
-                          child: RTCVideoView(_controller.localRenderer,
-                              mirror: true),
-                        ),
-                        Expanded(
-                          child: RTCVideoView(_controller.remoteRenderer),
-                        ),
-                      ],
+        body: SafeArea(
+          child: Container(
+            color: Colors.black.withOpacity(0.75),
+            child: Stack(
+              children: [
+                Positioned.fill(
+                  top: -100,
+                  child: RTCVideoView(_controller.remoteRenderer),
+                ),
+                Positioned(
+                  left: _localVideoOffset.dx,
+                  top: _localVideoOffset.dy,
+                  width: 100,
+                  height: 150,
+                  child: Draggable(
+                    feedback: Container(
+                      width: 100,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.greenAccent, width: 4),
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child:
+                          RTCVideoView(_controller.localRenderer, mirror: true),
+                    ),
+                    childWhenDragging: Container(),
+                    onDragEnd: (details) {
+                      final dragDistance =
+                          (details.offset - _localVideoOffset).distance;
+                      if (dragDistance > 10) {
+                        // Threshold to ignore minor taps
+                        setState(() {
+                          final newOffset = details.offset;
+                          final screenHeight =
+                              MediaQuery.of(context).size.height;
+                          const bottomButtonHeight =
+                              130.0; // Adjust this value as needed
+
+                          if (newOffset.dy + 150 >
+                              screenHeight - bottomButtonHeight) {
+                            _localVideoOffset = Offset(newOffset.dx,
+                                screenHeight - bottomButtonHeight - 150);
+                          } else {
+                            _localVideoOffset = newOffset;
+                          }
+                        });
+                      }
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.greenAccent, width: 4),
+                        borderRadius: BorderRadius.circular(8),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(0, 3),
+                          ),
+                        ],
+                      ),
+                      child:
+                          RTCVideoView(_controller.localRenderer, mirror: true),
                     ),
                   ),
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                IconButton(
-                  icon: Icon(_controller.isMuted ? Icons.mic_off : Icons.mic),
-                  onPressed: _onToggleMute,
                 ),
-                IconButton(
-                  icon: Icon(_controller.isCameraOff
-                      ? Icons.videocam_off
-                      : Icons.videocam),
-                  onPressed: _onToggleCamera,
-                ),
-                IconButton(
-                  icon: const Icon(Icons.call_end),
-                  onPressed: _onEndCall,
+                Positioned(
+                  bottom: 30,
+                  left: 20,
+                  right: 20,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      CircleButton(
+                        icon: Icon(
+                            _controller.isMuted ? Icons.mic_off : Icons.mic),
+                        onTap: _onToggleMute,
+                        buttonColor: Colors.white,
+                        buttonSize: 64,
+                        borderColor: Colors.grey,
+                      ),
+                      CircleButton(
+                        icon: Icon(_controller.isCameraOff
+                            ? Icons.videocam_off
+                            : Icons.videocam),
+                        onTap: _onToggleCamera,
+                        buttonColor: Colors.white,
+                        buttonSize: 64,
+                        borderColor: Colors.grey,
+                      ),
+                      CircleButton(
+                        icon: const Icon(Icons.call_end),
+                        onTap: _onEndCall,
+                        buttonColor: Colors.red,
+                        buttonSize: 64,
+                        borderColor: Colors.grey,
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-          ],
+          ),
         ),
       );
     }
