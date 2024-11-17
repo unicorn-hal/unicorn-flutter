f4b12d0a78b1d6f5d527c18a51362a6bf589e79f
RyuDra131 <ryunosuke.13043702@gmail.com>
2024-11-18 03:12:02 +0900

    wip: submitの処理追加

--- lib/Controller/Component/Pages/Register/register_user_info_controller.dart
+++ lib/Controller/Component/Pages/Register/register_user_info_controller.dart
@@ -3,8 +3,12 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:unicorn_flutter/Controller/Component/Pages/Register/register_physical_info_controller.dart';
 import 'package:unicorn_flutter/Controller/Core/controller_core.dart';
 import 'package:unicorn_flutter/Model/Data/User/user_data.dart';
+import 'package:unicorn_flutter/Model/Entity/User/physical_info.dart';
+import 'package:unicorn_flutter/Model/Entity/User/user.dart';
+import 'package:unicorn_flutter/Model/Entity/address_info.dart';
 import 'package:unicorn_flutter/Service/Firebase/CloudStorage/cloud_storage_service.dart';
 import 'package:unicorn_flutter/Service/Log/log_service.dart';
 import 'package:unicorn_flutter/Service/Package/ImageUtils/image_utils_service.dart';
@@ -12,10 +16,20 @@ import 'package:unicorn_flutter/View/bottom_navigation_bar_view.dart';
 import 'package:unicorn_flutter/gen/assets.gen.dart';
 
 class RegisterUserInfoController extends ControllerCore {
-
-  final TextEditingController phoneNumberTextController = TextEditingController();
+  final TextEditingController phoneNumberTextController =
+      TextEditingController();
   final TextEditingController emailTextController = TextEditingController();
-  final TextEditingController occupationTextController = TextEditingController();
+  final TextEditingController occupationTextController =
+      TextEditingController();
+
+  final RegisterPhysicalInfoController registerPhysicalInfoController =
+      RegisterPhysicalInfoController();
+  final RegisterAddressInfoController registerAddressInfoController =
+      RegisterAddressInfoController();
+
+  // RegisterUserInfoController(
+  //     {required this.registerPhysicalInfoController,
+  //     required this.registerAddressInfoController});
 
   FirebaseCloudStorageService get _cloudStorageService =>
       FirebaseCloudStorageService();
@@ -60,11 +74,34 @@ class RegisterUserInfoController extends ControllerCore {
     }
   }
 
-  Future<void> submit() async {
+  User submit() {
     if (_imageFile != null) {
-      await _uploadImage();
+      _uploadImage();
     }
-    // todo: APIでユーザー情報を登録する処理を追加
+
+    final AddressInfo? addressInfoSubmitValue =
+        registerAddressInfoController.submit();
+    final PhysicalInfo? physicalInfoSubmitValue =
+        registerPhysicalInfoController.submit();
+
+    User? user = User(
+      userId: '',
+      firstName: physicalInfoSubmitValue!.firstName,
+      lastName: physicalInfoSubmitValue.lastName,
+      email: emailTextController.text.trim(),
+      gender: physicalInfoSubmitValue.gender,
+      birthDate: physicalInfoSubmitValue.birthDate,
+      address:
+          "${addressInfoSubmitValue!.prefecture} ${addressInfoSubmitValue.city} ${addressInfoSubmitValue.town}",
+      postalCode: addressInfoSubmitValue.postalCode,
+      phoneNumber: phoneNumberTextController.text.trim(),
+      iconImageUrl: '',
+      bodyHeight: physicalInfoSubmitValue.bodyHeight,
+      bodyWeight: physicalInfoSubmitValue.bodyWeight,
+      occupation: occupationTextController.text.trim(),
+    );
+
+    return user;
   }
 
   bool validateField() {
@@ -72,9 +109,7 @@ class RegisterUserInfoController extends ControllerCore {
     phoneNumberTextController.text.isEmpty
         ? emptyMessageField.add("電話番号")
         : null;
-    emailTextController.text.isEmpty
-        ? emptyMessageField.add("メールアドレス")
-        : null;
+    emailTextController.text.isEmpty ? emptyMessageField.add("メールアドレス") : null;
     occupationTextController.text.isEmpty ? emptyMessageField.add("性別") : null;
 
     if (emptyMessageField.isNotEmpty) {
