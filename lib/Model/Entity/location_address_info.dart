ad68dc9d613df80e0ee8f072aeecdcb8cca6eb68
RyuDra131 <ryunosuke.13043702@gmail.com>
2024-11-18 04:43:45 +0900

    Merge commit 'ae782273b82c6b05e6c162befdd34304848b6612' into feature/Controller/Profile/user_info
    # Conflicts:
    #	lib/View/Component/Pages/Register/register_user_info_view.dart

--- lib/Model/Entity/location_address_info.dart
+++ lib/Model/Entity/location_address_info.dart
@@ -1,18 +1,18 @@
-class AddressInfo {
+class LocationAddressInfo {
   final String postalCode;
   final String prefecture;
   final String city;
   final String town;
 
-  AddressInfo({
+  LocationAddressInfo({
     required this.postalCode,
     required this.prefecture,
     required this.city,
     required this.town,
   });
 
-  factory AddressInfo.fromJson(Map<String, dynamic> json) {
-    return AddressInfo(
+  factory LocationAddressInfo.fromJson(Map<String, dynamic> json) {
+    return LocationAddressInfo(
       postalCode: json['postalCode'],
       prefecture: json['prefecture'],
       city: json['city'],
