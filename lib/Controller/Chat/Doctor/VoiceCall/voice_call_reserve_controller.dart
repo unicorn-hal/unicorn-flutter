1a6679a2157eebe9b48b192911a94c1fe5361ef3
ishibashikeita <ishibashi.hnut@gmail.com>
2024-11-21 20:53:08 +0900

    fix: 予約カレンダーを表示まで

--- lib/Controller/Chat/Doctor/VoiceCall/voice_call_reserve_controller.dart
+++ lib/Controller/Chat/Doctor/VoiceCall/voice_call_reserve_controller.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:intl/intl.dart';
+import 'package:unicorn_flutter/Constants/strings.dart';
 import 'package:unicorn_flutter/Route/router.dart';
 import 'package:unicorn_flutter/View/bottom_navigation_bar_view.dart';
 
 import '../../../../Model/Data/Account/account_data.dart';
+import '../../../../Model/Entity/Call/call.dart';
 import '../../../../Model/Entity/Call/call_request.dart';
+import '../../../../Model/Entity/Doctor/doctor.dart';
 import '../../../../Service/Api/Call/call_api.dart';
 import '../../../Core/controller_core.dart';
 
 class VoiceCallReserveController extends ControllerCore {
   CallApi get _callApi => CallApi();
 
-  VoiceCallReserveController(this.context, this.doctorId, this.doctorName);
+  VoiceCallReserveController(this.context, this.doctor);
 
   BuildContext? context;
-  final String doctorId;
-  final String doctorName;
+  final Doctor doctor;
 
   late DateTime _reserveDate;
   late DateTime _reserveTime;
+  DateTime _calendarDate =
+      DateTime(DateTime.now().year, DateTime.now().month, DateTime.now().day);
 
   @override
   void initialize() async {
@@ -40,7 +45,7 @@ class VoiceCallReserveController extends ControllerCore {
     );
 
     CallRequest body = CallRequest(
-      doctorId: doctorId,
+      doctorId: doctor.doctorId,
       userId: AccountData().account!.uid,
       callStartTime: reserveDateTime,
       // 一旦固定で30分間に設定
@@ -61,10 +66,13 @@ class VoiceCallReserveController extends ControllerCore {
     }
 
     // 通話予約に成功した場合
+    final String reserveMessage =
+        '通話予約が完了しました！ \n\n■予約内容\n${DateFormat('yyyy年MM月dd日').format(reserveDateTime)}\n\n■時間\n${DateFormat('HH:mm').format(reserveDateTime)}〜${DateFormat('HH:mm').format(reserveDateTime.add(const Duration(minutes: 10)))}\n\n■医師名\n${doctor.lastName + doctor.firstName}\n\n■その他\n通話・予約に関してのお問い合わせはアプリ内「プロフィール > お問い合わせ」からお申し上げください。';
+
     ChatDoctorTextChatRoute(
-            doctorId: doctorId,
-            doctorName: doctorName,
-            reserveMessage: '通話予約が完了しました > ${reserveDateTime}')
+            doctorId: doctor.doctorId,
+            doctorName: doctor.lastName + doctor.firstName,
+            reserveMessage: reserveMessage)
         .push(context!);
     return;
   }
@@ -78,4 +86,47 @@ class VoiceCallReserveController extends ControllerCore {
   void changeTime(DateTime time) {
     _reserveTime = time;
   }
+
+  // カレンダーの日付変更
+  void changeCalendarDate(DateTime date) {
+    _calendarDate = date;
+  }
+
+  Future<Map<DateTime, List<Call>>?> getDoctorReservation() async {
+    final List<Call>? callList = await _callApi.getCall(
+        doctorId: doctor.doctorId, userId: AccountData().account!.uid);
+
+    if (callList == null) {
+      Fluttertoast.showToast(msg: Strings.ERROR_RESPONSE_TEXT);
+      return null;
+    }
+
+    final Map<DateTime, List<Call>> events = {};
+
+    // 予約情報をカレンダーに表示する形に変換
+    for (final Call call in callList) {
+      // 予約日時の日付部分を取得
+      final DateTime callDate = DateTime(
+        call.callStartTime.year,
+        call.callStartTime.month,
+        call.callStartTime.day,
+      );
+
+      // 予約日時の日付がカレンダーの日付と一致する場合
+      if (events.containsKey(callDate)) {
+        events[callDate]!.add(call);
+      } else {
+        events[callDate] = [call];
+      }
+    }
+
+    return events;
+  }
+
+  DateTime normalizeDate(DateTime date) {
+    final localDate = date.toLocal();
+    return DateTime(localDate.year, localDate.month, localDate.day);
+  }
+
+  DateTime get calendarDate => _calendarDate;
 }
--- lib/View/Chat/Doctor/VoiceCall/Reserve/voice_call_reserve_view.dart
+++ lib/View/Chat/Doctor/VoiceCall/Reserve/voice_call_reserve_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:intl/intl.dart';
+import 'package:table_calendar/table_calendar.dart';
 import 'package:unicorn_flutter/Controller/Chat/Doctor/VoiceCall/voice_call_reserve_controller.dart';
+import 'package:unicorn_flutter/Service/Log/log_service.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_appbar.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_drum_roll.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_scaffold.dart';
@@ -7,6 +11,7 @@ import 'package:unicorn_flutter/View/Component/CustomWidget/custom_text.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/spacer_and_divider.dart';
 import 'package:unicorn_flutter/gen/colors.gen.dart';
 
+import '../../../../../Model/Entity/Call/call.dart';
 import '../../../../../Model/Entity/Doctor/doctor.dart';
 
 class VoiceCallReserveView extends StatefulWidget {
@@ -27,8 +32,7 @@ class _VoiceCallReserveViewState extends State<VoiceCallReserveView> {
   @override
   void initState() {
     super.initState();
-    controller = VoiceCallReserveController(context, widget.doctor.doctorId,
-        widget.doctor.lastName + widget.doctor.firstName);
+    controller = VoiceCallReserveController(context, widget.doctor);
   }
 
   @override
@@ -67,48 +71,144 @@ class _VoiceCallReserveViewState extends State<VoiceCallReserveView> {
                 ),
               ),
             ),
-            Container(
-              width: size.width * 0.9,
-              height: 140,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: ColorName.shadowGray,
-                  width: 2,
+            Stack(
+              children: [
+                Container(
+                  width: size.width * 0.9,
+                  height: 140,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: ColorName.shadowGray,
+                      width: 2,
+                    ),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 8.0,
+                          horizontal: 16.0,
+                        ),
+                        child: CustomText(
+                          text: '対応可能時間',
+                          fontSize: 18,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8.0,
+                          horizontal: 16.0,
+                        ),
+                        child: CustomText(
+                            text: 'チャット: ${widget.doctor.chatSupportHours}'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 8.0,
+                          horizontal: 16.0,
+                        ),
+                        child: CustomText(
+                          text: '通話: ${widget.doctor.callSupportHours}',
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.symmetric(
-                      vertical: 8.0,
-                      horizontal: 16.0,
-                    ),
-                    child: CustomText(
-                      text: '対応可能時間',
-                      fontSize: 18,
+                Positioned(
+                  bottom: 10,
+                  right: 10,
+                  child: GestureDetector(
+                    onTap: () async {
+                      final Map<DateTime, List<Call>>? events =
+                          await controller.getDoctorReservation();
+
+                      if (events == null) {
+                        return;
+                      }
+
+                      showDialog(
+                          context: context,
+                          builder: (_) {
+                            return Column(
+                              children: [
+                                Dialog(
+                                  backgroundColor: Colors.white,
+                                  surfaceTintColor: Colors.white,
+                                  child: StatefulBuilder(
+                                      builder: (context, setState) {
+                                    return TableCalendar(
+                                      currentDay: controller.calendarDate,
+                                      focusedDay: DateTime.now(),
+                                      firstDay: DateTime.now().add(
+                                        const Duration(days: -365),
+                                      ),
+                                      lastDay: DateTime.now()
+                                          .add(const Duration(days: 365)),
+                                      calendarFormat: CalendarFormat.month,
+                                      headerStyle: const HeaderStyle(
+                                        formatButtonVisible: false,
+                                        leftChevronIcon: Icon(
+                                          Icons.chevron_left,
+                                          color: ColorName.mainColor,
+                                        ),
+                                        rightChevronIcon: Icon(
+                                          Icons.chevron_right,
+                                          color: ColorName.mainColor,
+                                        ),
+                                      ),
+                                      onDaySelected: (DateTime selectedDay,
+                                          DateTime focusedDay) {
+                                        controller
+                                            .changeCalendarDate(selectedDay);
+                                        setState(() {});
+                                      },
+                                      selectedDayPredicate: (day) {
+                                        return isSameDay(
+                                            controller.calendarDate, day);
+                                      },
+                                      eventLoader: (day) {
+                                        final DateTime targetDate = DateTime(
+                                          day.year,
+                                          day.month,
+                                          day.day,
+                                        );
+                                        return events[targetDate] ?? [];
+                                      },
+                                    );
+                                  }),
+                                ),
+                                Expanded(
+                                    child: ListView.builder(
+                                        itemCount: events[controller
+                                                    .normalizeDate(controller
+                                                        .calendarDate)]
+                                                ?.length ??
+                                            0,
+                                        itemBuilder: (context, index) {
+                                          return Container(
+                                            width: 100,
+                                            height: 50,
+                                            color: Colors.red,
+                                          );
+                                        }))
+                              ],
+                            );
+                          });
+                    },
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: ColorName.mainColor),
+                      child: const Icon(Icons.calendar_month,
+                          color: Colors.white, size: 30),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 8.0,
-                      horizontal: 16.0,
-                    ),
-                    child: CustomText(
-                        text: 'チャット: ${widget.doctor.chatSupportHours}'),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 8.0,
-                      horizontal: 16.0,
-                    ),
-                    child: CustomText(
-                      text: '通話: ${widget.doctor.callSupportHours}',
-                    ),
-                  ),
-                ],
-              ),
+                )
+              ],
             ),
             const Padding(
               padding: EdgeInsets.symmetric(
