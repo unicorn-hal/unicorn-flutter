126765f9ff3984084e0f0f0202c7a76d98843ced
kakeru-ikeda <kakerumail1451@gmail.com>
2024-11-22 21:00:12 +0900

    wip: Agoraトークンの取得機能を追加し、ボイスチャットの初期化時に使用

--- lib/Controller/Chat/Doctor/VoiceCall/voice_call_controller.dart
+++ lib/Controller/Chat/Doctor/VoiceCall/voice_call_controller.dart
@@ -3,9 +3,11 @@
 import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:flutter/foundation.dart';
 import 'package:unicorn_flutter/Controller/Core/controller_core.dart';
-import 'package:unicorn_flutter/Model/Data/User/user_data.dart';
+import 'package:unicorn_flutter/Model/Entity/Agora/agora_certificate.dart';
 import 'package:unicorn_flutter/Model/Entity/Doctor/doctor.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:unicorn_flutter/Model/Entity/Agora/agora_certificate_request.dart';
+import 'package:unicorn_flutter/Service/Package/Agora/Api/agora_certificate_api.dart';
 
 class VoiceCallController extends ControllerCore {
   final Doctor _doctor;
@@ -13,6 +15,7 @@ class VoiceCallController extends ControllerCore {
   late RtcEngine _engine;
   bool isMuted = false;
   bool isSwapped = false;
+  String? _token;
 
   final ValueNotifier<bool> _isCallConnected = ValueNotifier(false);
 
@@ -22,9 +25,12 @@ class VoiceCallController extends ControllerCore {
   RtcEngine get engine => _engine;
   ValueNotifier<bool> get isCallConnected => _isCallConnected;
 
+  final AgoraCertificateApi _agoraApi = AgoraCertificateApi();
+
   @override
   Future<void> initialize() async {
     await _initAgoraEngine();
+    await _fetchToken();
     await _joinChannel();
   }
 
@@ -50,10 +56,24 @@ class VoiceCallController extends ControllerCore {
     await _engine.enableVideo();
   }
 
+  Future<void> _fetchToken() async {
+    AgoraCertificateRequest request = AgoraCertificateRequest(
+      channelName: 'test',
+      uid: '0',
+    );
+    AgoraCertificate? certificate = await _agoraApi.fetchToken(body: request);
+    if (certificate != null) {
+      _token = certificate.token;
+    } else {
+      // トークン取得に失敗した場合の処理
+      _token = '';
+    }
+  }
+
   Future<void> _joinChannel() async {
     await _engine.startPreview();
     await _engine.joinChannel(
-      token: dotenv.env['AGORA_TOKEN']!,
+      token: _token!,
       channelId: 'test',
       uid: 0,
       options: const ChannelMediaOptions(),
@@ -79,7 +99,6 @@ class VoiceCallController extends ControllerCore {
     _isCallConnected.value = false;
   }
 
-  @override
   void dispose() {
     _engine.leaveChannel();
     _engine.release();
