48b20a3ee5d0e142ca33a3f5e7fede5c55338491
kakeru-ikeda <kakerumail1451@gmail.com>
2024-11-24 02:09:34 +0900

    fix: uidはサーバー発行されるものを利用するようにModelを変更

--- lib/Model/Entity/Agora/agora_certificate.dart
+++ lib/Model/Entity/Agora/agora_certificate.dart
@@ -1,16 +1,19 @@
 class AgoraCertificate {
+  final int uid;
   final String token;
 
-  AgoraCertificate({required this.token});
+  AgoraCertificate({required this.uid, required this.token});
 
   factory AgoraCertificate.fromJson(Map<String, dynamic> json) {
     return AgoraCertificate(
+      uid: json['uid'],
       token: json['token'],
     );
   }
 
   Map<String, dynamic> toJson() {
     return {
+      'uid': uid,
       'token': token,
     };
   }
