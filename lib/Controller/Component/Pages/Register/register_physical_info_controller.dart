3bb5f853ef30420040cf13a9e026ec0760c93369
RyuDra131 <ryunosuke.13043702@gmail.com>
2024-11-25 08:06:00 +0900

    add: PhysicalInfoControllerにput処理追加

--- lib/Controller/Component/Pages/Register/register_physical_info_controller.dart
+++ lib/Controller/Component/Pages/Register/register_physical_info_controller.dart
@@ -1,10 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:unicorn_flutter/Constants/Enum/user_gender_enum.dart';
 import 'package:unicorn_flutter/Controller/Core/controller_core.dart';
 import 'package:unicorn_flutter/Model/Data/User/user_data.dart';
 import 'package:unicorn_flutter/Model/Entity/User/physical_info.dart';
+import 'package:unicorn_flutter/Model/Entity/User/user.dart';
 import 'package:unicorn_flutter/Model/Entity/User/user_request.dart';
+import 'package:unicorn_flutter/Service/Api/User/user_api.dart';
 
 class RegisterPhysicalInfoController extends ControllerCore {
   late DateTime birthDate;
@@ -21,6 +25,37 @@ class RegisterPhysicalInfoController extends ControllerCore {
   @override
   void initialize() {
     birthDate = DateTime.now();
+  }
+
+  Future<void> setValuesFromGetUser(UserData userData) async {
+    await UserApi().getUser(userId: userData.user!.userId);
+
+    PhysicalInfo physicalInfo = PhysicalInfo(
+      firstName: firstNameTextController.text,
+      lastName: lastNameTextController.text,
+      gender: gender!,
+      birthDate: birthDate,
+      bodyHeight: double.tryParse(bodyHeightTextController.text)!,
+      bodyWeight: double.tryParse(bodyWeightTextController.text)!,
+    );
+
+    UserRequest userRequest = UserRequest(
+      userId: userData.user!.userId,
+      firstName: physicalInfo.firstName,
+      lastName: physicalInfo.lastName,
+      email: userData.user!.email,
+      gender: physicalInfo.gender,
+      birthDate: physicalInfo.birthDate,
+      address: userData.user!.address,
+      postalCode: userData.user!.postalCode,
+      phoneNumber: userData.user!.phoneNumber,
+      iconImageUrl: userData.user!.iconImageUrl,
+      bodyHeight: physicalInfo.bodyHeight,
+      bodyWeight: physicalInfo.bodyWeight,
+      occupation: userData.user!.occupation,
+    );
+
+    UserApi().putUser(userId: userData.user!.userId, body: userRequest);
   }
 
   UserRequest? submit() {
