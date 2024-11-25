9b2ac29dd401f53fdd5875baa8de9d94f278c786
RyuDra131 <ryunosuke.13043702@gmail.com>
2024-11-26 07:17:51 +0900

    add: routesによる処理分岐追加

--- lib/Controller/Component/Pages/Register/register_physical_info_controller.dart
+++ lib/Controller/Component/Pages/Register/register_physical_info_controller.dart
@@ -1,69 +1,61 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:unicorn_flutter/Constants/Enum/user_gender_enum.dart';
+import 'package:unicorn_flutter/Constants/strings.dart';
 import 'package:unicorn_flutter/Controller/Core/controller_core.dart';
 import 'package:unicorn_flutter/Model/Data/User/user_data.dart';
 import 'package:unicorn_flutter/Model/Entity/User/physical_info.dart';
-import 'package:unicorn_flutter/Model/Entity/User/user.dart';
 import 'package:unicorn_flutter/Model/Entity/User/user_request.dart';
+import 'package:unicorn_flutter/Route/router.dart';
+import 'package:unicorn_flutter/Route/routes.dart';
 import 'package:unicorn_flutter/Service/Api/User/user_api.dart';
 
 class RegisterPhysicalInfoController extends ControllerCore {
+  RegisterPhysicalInfoController({
+    required this.context,
+    required super.from,
+  });
+
+  TextEditingController firstNameTextController = TextEditingController();
+  TextEditingController lastNameTextController = TextEditingController();
+  TextEditingController bodyHeightTextController = TextEditingController();
+  TextEditingController bodyWeightTextController = TextEditingController();
+
   late DateTime birthDate;
   UserGenderEnum? gender;
+  BuildContext context;
   UserData userData = UserData();
-
-  final TextEditingController firstNameTextController = TextEditingController();
-  final TextEditingController lastNameTextController = TextEditingController();
-  final TextEditingController bodyHeightTextController =
-      TextEditingController();
-  final TextEditingController bodyWeightTextController =
-      TextEditingController();
 
   @override
   void initialize() {
     birthDate = DateTime.now();
   }
 
-  Future<void> setValuesFromGetUser(UserData userData) async {
-    await UserApi().getUser(userId: userData.user!.userId);
-
-    PhysicalInfo physicalInfo = PhysicalInfo(
-      firstName: firstNameTextController.text,
-      lastName: lastNameTextController.text,
-      gender: gender!,
-      birthDate: birthDate,
-      bodyHeight: double.tryParse(bodyHeightTextController.text)!,
-      bodyWeight: double.tryParse(bodyWeightTextController.text)!,
-    );
-
-    UserRequest userRequest = UserRequest(
-      userId: userData.user!.userId,
-      firstName: physicalInfo.firstName,
-      lastName: physicalInfo.lastName,
-      email: userData.user!.email,
-      gender: physicalInfo.gender,
-      birthDate: physicalInfo.birthDate,
-      address: userData.user!.address,
-      postalCode: userData.user!.postalCode,
-      phoneNumber: userData.user!.phoneNumber,
-      iconImageUrl: userData.user!.iconImageUrl,
-      bodyHeight: physicalInfo.bodyHeight,
-      bodyWeight: physicalInfo.bodyWeight,
-      occupation: userData.user!.occupation,
-    );
-
-    UserApi().putUser(userId: userData.user!.userId, body: userRequest);
+  void setTextEditingController() {
+    if (from == Routes.profile) {
+      firstNameTextController =
+          TextEditingController(text: userData.user!.firstName);
+      lastNameTextController =
+          TextEditingController(text: userData.user!.lastName);
+      gender = userData.user!.gender;
+      birthDate = userData.user!.birthDate;
+      bodyHeightTextController =
+          TextEditingController(text: userData.user!.bodyHeight.toString());
+      bodyWeightTextController =
+          TextEditingController(text: userData.user!.bodyWeight.toString());
+    } else {
+      return;
+    }
   }
 
-  UserRequest? submit() {
+  Future<UserRequest?> submit(UserRequest userRequest) async {
     if (validateField() == false) {
       return null;
     }
 
-    // todo: 編集処理でき次第、修正加えます。
     PhysicalInfo physicalInfo = PhysicalInfo(
       firstName: firstNameTextController.text,
       lastName: lastNameTextController.text,
@@ -72,6 +64,38 @@ class RegisterPhysicalInfoController extends ControllerCore {
       bodyHeight: double.tryParse(bodyHeightTextController.text)!,
       bodyWeight: double.tryParse(bodyWeightTextController.text)!,
     );
+
+    if (from == Routes.profile) {
+      UserRequest userRequest = UserRequest(
+        userId: userData.user!.userId,
+        firstName: physicalInfo.firstName,
+        lastName: physicalInfo.lastName,
+        email: userData.user!.email,
+        gender: physicalInfo.gender,
+        birthDate: physicalInfo.birthDate,
+        address: userData.user!.address,
+        postalCode: userData.user!.postalCode,
+        phoneNumber: userData.user!.phoneNumber,
+        iconImageUrl: userData.user!.iconImageUrl,
+        bodyHeight: physicalInfo.bodyHeight,
+        bodyWeight: physicalInfo.bodyWeight,
+        occupation: userData.user!.occupation,
+      );
+      Future<int> responceCode =
+          UserApi().putUser(userId: userData.user!.userId, body: userRequest);
+      if (await responceCode == 200) {
+        Fluttertoast.showToast(msg: Strings.PROFILE_EDIT_COMPLETED_TEXT);
+        return ProfileRoute().push(context);
+      }
+      if (await responceCode == 400) {
+        Fluttertoast.showToast(msg: Strings.ERROR_RESPONSE_TEXT);
+        return ProfileRoute().push(context);
+      }
+      if (await responceCode == 500) {
+        Fluttertoast.showToast(msg: Strings.ERROR_RESPONSE_TEXT);
+        return ProfileRoute().push(context);
+      }
+    }
 
     UserRequest? userRequest = UserRequest(
       firstName: physicalInfo.firstName,
