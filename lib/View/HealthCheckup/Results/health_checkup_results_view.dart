65b5212cc305f7bdf4a30081c29aa8e31a5bbf2a
kakeru-ikeda <kakerumail1451@gmail.com>
2024-11-27 14:02:22 +0900

    fix: HealthCheckupResultsControllerのインポートパスを修正

--- lib/View/HealthCheckup/Results/health_checkup_results_view.dart
+++ lib/View/HealthCheckup/Results/health_checkup_results_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:unicorn_flutter/Constants/Enum/health_checkup_disease_enum.dart';
-import 'package:unicorn_flutter/Controller/HealthCheckup/health_checkup_results_controller.dart';
+import 'package:unicorn_flutter/Controller/HealthCheckup/Results/health_checkup_results_controller.dart';
 import 'package:unicorn_flutter/Model/Data/User/user_data.dart';
 import 'package:unicorn_flutter/Route/router.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_button.dart';
