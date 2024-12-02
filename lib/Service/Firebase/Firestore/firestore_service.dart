bc115d36cd1d8d882b5731625a8087af37b802bf
kakeru-ikeda <kakerumail1451@gmail.com>
2024-12-02 23:07:14 +0900

    fix: FirestoreServiceのdispose処理を強化

--- lib/Service/Firebase/Firestore/firestore_service.dart
+++ lib/Service/Firebase/Firestore/firestore_service.dart
@@ -2,42 +2,46 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 class FirebaseFirestoreService {
-  late FirebaseFirestore _firestore;
+  late FirebaseFirestore _instance;
 
   FirebaseFirestoreService({String databaseId = '(default)'}) {
-    _firestore = FirebaseFirestore.instanceFor(
+    _instance = FirebaseFirestore.instanceFor(
       app: Firebase.app(),
       databaseId: databaseId,
     );
   }
 
   Future<void> addData(String collection, Map<String, dynamic> data) async {
-    await _firestore.collection(collection).add(data);
+    await _instance.collection(collection).add(data);
   }
 
   Future<void> updateData(
       String collection, String id, Map<String, dynamic> data) async {
-    await _firestore.collection(collection).doc(id).update(data);
+    await _instance.collection(collection).doc(id).update(data);
   }
 
   Future<void> deleteData(String collection, String id) async {
-    await _firestore.collection(collection).doc(id).delete();
+    await _instance.collection(collection).doc(id).delete();
   }
 
   Future<DocumentSnapshot> getData(String collection, String id) async {
-    return await _firestore.collection(collection).doc(id).get();
+    return await _instance.collection(collection).doc(id).get();
   }
 
   Future<List<DocumentSnapshot>> getCollectionData(String collection) async {
-    final snapshot = await _firestore.collection(collection).get();
+    final snapshot = await _instance.collection(collection).get();
     return snapshot.docs;
   }
 
   Stream<QuerySnapshot> streamData(String collection) {
-    return _firestore.collection(collection).snapshots();
+    return _instance.collection(collection).snapshots(
+        includeMetadataChanges: true, source: ListenSource.defaultSource);
   }
 
-  void dispose() {
-    _firestore.terminate();
+  void dispose() async {
+    _instance.terminate();
+    await _instance.clearPersistence();
+    await _instance.disableNetwork();
+    await _instance.enableNetwork();
   }
 }
