0f143f2d8f48c74d18ec2c9bb567c8e86bcc6930
LAPTOP-537VN1FQ\rei20 <nakada0404rei@gmail.com>
2024-12-06 22:01:03 +0900

    add: 通話予約を削除する関数を追加

--- lib/Controller/Profile/CallReservation/call_reservation_controller.dart
+++ lib/Controller/Profile/CallReservation/call_reservation_controller.dart
@@ -1,11 +1,16 @@
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:unicorn_flutter/Constants/strings.dart';
 import 'package:unicorn_flutter/Controller/Core/controller_core.dart';
 import 'package:unicorn_flutter/Model/Data/User/user_data.dart';
 import 'package:unicorn_flutter/Model/Entity/Call/call_reservation.dart';
+import 'package:unicorn_flutter/Service/Api/Call/call_api.dart';
 import 'package:unicorn_flutter/Service/Api/User/user_api.dart';
+import 'package:unicorn_flutter/View/bottom_navigation_bar_view.dart';
 
 class CallReservationController extends ControllerCore {
   /// Serviceのインスタンス化
   UserApi get _userApi => UserApi();
+  CallApi get _callApi => CallApi();
 
   /// コンストラクタ
   CallReservationController();
@@ -15,9 +20,21 @@ class CallReservationController extends ControllerCore {
   void initialize() {}
 
   /// 各関数の実装
+  /// 通話予約のリストを取得する関数
   Future<List<CallReservation>?> getCallReservation() async {
     List<CallReservation>? res =
         await _userApi.getUserCallReservation(userId: UserData().user!.userId);
+    return res;
+  }
+
+  /// 通話予約を削除する関数
+  Future<int> deleteCallReservation(String callReservationId) async {
+    ProtectorNotifier().enableProtector();
+    int res = await _callApi.deleteCall(callReservationId: callReservationId);
+    if (res != 204) {
+      Fluttertoast.showToast(msg: Strings.ERROR_RESPONSE_TEXT);
+    }
+    ProtectorNotifier().disableProtector();
     return res;
   }
 }
