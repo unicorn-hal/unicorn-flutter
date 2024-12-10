f3dbe27b9d486c5ed61d8365e66d7fad374c70cf
kakeru-ikeda <kakerumail1451@gmail.com>
2024-12-10 21:40:35 +0900

    feat: GoogleMapViewerにUnicornの現在地を表示する機能を追加

--- lib/View/Component/Parts/google_map_viewer.dart
+++ lib/View/Component/Parts/google_map_viewer.dart
@@ -11,9 +11,11 @@ class GoogleMapViewer extends StatefulWidget {
     super.key,
     required this.point,
     this.destination,
+    this.current,
   });
   final LatLng point;
   final LatLng? destination;
+  final LatLng? current;
 
   @override
   State<GoogleMapViewer> createState() => _GoogleMapViewerState();
@@ -24,6 +26,7 @@ class _GoogleMapViewerState extends State<GoogleMapViewer> {
 
   late LatLng _point;
   late LatLng? _destination;
+  late LatLng? _current;
 
   final Set<Polyline> _polylines = {};
 
@@ -32,6 +35,7 @@ class _GoogleMapViewerState extends State<GoogleMapViewer> {
     super.initState();
     _point = widget.point;
     _destination = widget.destination;
+    _current = widget.current;
 
     if (_destination != null) {
       _fetchRoute();
@@ -42,10 +46,12 @@ class _GoogleMapViewerState extends State<GoogleMapViewer> {
   void didUpdateWidget(GoogleMapViewer oldWidget) {
     super.didUpdateWidget(oldWidget);
     if (widget.point != oldWidget.point ||
-        widget.destination != oldWidget.destination) {
+        widget.destination != oldWidget.destination ||
+        widget.current != oldWidget.current) {
       setState(() {
         _point = widget.point;
         _destination = widget.destination;
+        _current = widget.current;
         _polylines.clear();
       });
       if (_destination != null) {
@@ -53,7 +59,7 @@ class _GoogleMapViewerState extends State<GoogleMapViewer> {
         _animateCameraToBounds();
       } else {
         try {
-          mapController!.animateCamera(
+          mapController?.animateCamera(
             CameraUpdate.newLatLng(_point),
           );
         } catch (e) {
@@ -131,24 +137,32 @@ class _GoogleMapViewerState extends State<GoogleMapViewer> {
   void _animateCameraToBounds() {
     LatLngBounds bounds = LatLngBounds(
       southwest: LatLng(
-        _point.latitude < _destination!.latitude
-            ? _point.latitude
-            : _destination!.latitude,
-        _point.longitude < _destination!.longitude
-            ? _point.longitude
-            : _destination!.longitude,
+        [
+          _point.latitude,
+          _destination!.latitude,
+          _current?.latitude ?? _point.latitude
+        ].reduce((a, b) => a < b ? a : b),
+        [
+          _point.longitude,
+          _destination!.longitude,
+          _current?.longitude ?? _point.longitude
+        ].reduce((a, b) => a < b ? a : b),
       ),
       northeast: LatLng(
-        _point.latitude > _destination!.latitude
-            ? _point.latitude
-            : _destination!.latitude,
-        _point.longitude > _destination!.longitude
-            ? _point.longitude
-            : _destination!.longitude,
+        [
+          _point.latitude,
+          _destination!.latitude,
+          _current?.latitude ?? _point.latitude
+        ].reduce((a, b) => a > b ? a : b),
+        [
+          _point.longitude,
+          _destination!.longitude,
+          _current?.longitude ?? _point.longitude
+        ].reduce((a, b) => a > b ? a : b),
       ),
     );
     try {
-      mapController!.animateCamera(
+      mapController?.animateCamera(
         CameraUpdate.newLatLngBounds(bounds, 50),
       );
     } catch (e) {
@@ -182,6 +196,14 @@ class _GoogleMapViewerState extends State<GoogleMapViewer> {
               markerId: const MarkerId('destination'),
               position: _destination!,
               infoWindow: const InfoWindow(title: 'Destination'),
+            ),
+          if (_current != null)
+            Marker(
+              markerId: const MarkerId('current'),
+              position: _current!,
+              infoWindow: const InfoWindow(title: 'Current Location'),
+              icon: BitmapDescriptor.defaultMarkerWithHue(
+                  BitmapDescriptor.hueMagenta),
             ),
         },
       ),
