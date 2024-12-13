0631d2f6f5248635fa675fdbb8817daba8c9d733
LAPTOP-537VN1FQ\rei20 <nakada0404rei@gmail.com>
2024-12-13 23:55:34 +0900

    fix: headerTitleデザイン修正

--- lib/View/Component/Parts/header_title.dart
+++ lib/View/Component/Parts/header_title.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_text.dart';
+import 'package:unicorn_flutter/gen/colors.gen.dart';
 
 class HeaderTitle extends StatelessWidget {
   const HeaderTitle({
     super.key,
     required this.title,
-    this.fontSize = 20,
+    this.fontSize = 16,
     this.padding = const EdgeInsets.symmetric(
       vertical: 8.0,
       horizontal: 16.0,
@@ -24,7 +25,10 @@ class HeaderTitle extends StatelessWidget {
       padding: padding,
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.white,
+          color: ColorName.mainColor,
+          borderRadius: const BorderRadius.all(
+            Radius.circular(20),
+          ),
           border: useBorder
               ? const Border(
                   bottom: BorderSide(
@@ -34,9 +38,29 @@ class HeaderTitle extends StatelessWidget {
                 )
               : null,
         ),
-        child: CustomText(
-          text: title,
-          fontSize: fontSize,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 4,
+            horizontal: 8,
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Icon(
+                Icons.circle,
+                color: Colors.white,
+                size: 16,
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              CustomText(
+                text: title,
+                fontSize: fontSize,
+                color: Colors.white,
+              ),
+            ],
+          ),
         ),
       ),
     );
