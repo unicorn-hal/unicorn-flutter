9e7f474ee40bec2e8f1162e50aa88be9745538f1
LAPTOP-537VN1FQ\rei20 <nakada0404rei@gmail.com>
2024-12-14 01:05:47 +0900

    add: FieldTitle作成

---
+++ lib/View/Component/Parts/field_title.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:unicorn_flutter/View/Component/CustomWidget/custom_text.dart';
+import 'package:unicorn_flutter/gen/colors.gen.dart';
+
+class FieldTitle extends StatelessWidget {
+  const FieldTitle({
+    super.key,
+    required this.title,
+    this.fontSize = 16,
+    this.padding = EdgeInsets.zero,
+    this.icon = Icons.edit,
+    this.iconColor = ColorName.mainColor,
+  });
+
+  final String title;
+  final EdgeInsetsGeometry padding;
+  final double fontSize;
+  final IconData icon;
+  final Color iconColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: padding,
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(right: 5),
+              child: CustomText(
+                text: title,
+                fontSize: fontSize,
+              ),
+            ),
+            Icon(
+              icon,
+              color: iconColor,
+              size: 20,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
