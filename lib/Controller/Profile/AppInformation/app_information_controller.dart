dbab92f9c7e4c82a15007f52c24b4ec4539771c5
ryo hasegawa <minecraftnolisio@gmail.com>
2024-12-17 15:51:16 +0900

    add:app_information_view 退会追加

--- lib/Controller/Profile/AppInformation/app_information_controller.dart
+++ lib/Controller/Profile/AppInformation/app_information_controller.dart
@@ -1,11 +1,24 @@
 import 'package:unicorn_flutter/Controller/Core/controller_core.dart';
+import 'package:unicorn_flutter/Model/Data/User/user_data.dart';
+import 'package:unicorn_flutter/Route/router.dart';
+import 'package:unicorn_flutter/Service/Api/Account/account_api.dart';
+import 'package:unicorn_flutter/Service/Api/User/user_api.dart';
+import 'package:unicorn_flutter/Service/Firebase/Authentication/authentication_service.dart';
+import 'package:unicorn_flutter/Service/Package/SharedPreferences/shared_preferences_service.dart';
 import 'package:unicorn_flutter/Service/Package/SystemInfo/system_info_service.dart';
 import 'package:unicorn_flutter/Service/Package/UrlLauncher/url_launcher_service.dart';
+import 'package:unicorn_flutter/View/bottom_navigation_bar_view.dart';
 
 class AppInformationController extends ControllerCore {
   /// Serviceのインスタンス化
   UrlLauncherService get _urlLauncherService => UrlLauncherService();
   SystemInfoService get _systemInfoService => SystemInfoService();
+  UserApi get _userApi => UserApi();
+  AccountApi get _accountApi => AccountApi();
+  FirebaseAuthenticationService get _firebaseAuthenticationService =>
+      FirebaseAuthenticationService();
+  SharedPreferencesService get _sharedPreferencesService =>
+      SharedPreferencesService();
 
   /// 変数の定義
   // todo: url決まったら入れる
@@ -31,5 +44,16 @@ class AppInformationController extends ControllerCore {
   /// アプリのレビューページに飛ばす関数
   Future<void> openReview() async {
     await _systemInfoService.openReview();
+  }
+
+  Future<void> unsubscribe(context) async {
+    ProtectorNotifier().enableProtector();
+    _userApi.deleteUser(userId: UserData().user!.userId);
+    _accountApi.deleteAccount();
+    _firebaseAuthenticationService.signOut();
+    _sharedPreferencesService.clear();
+    print('発火してます');
+    ProtectorNotifier().disableProtector();
+    TopLoadingRoute().go(context);
   }
 }
