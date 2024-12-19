5f5ff82950502a3ff6d91b68723a4ed69328035e
kakeru-ikeda <kakerumail1451@gmail.com>
2024-12-19 17:28:09 +0900

    feat: TopLoadingViewにデモアプリの注意書きを追加

--- lib/View/top_loading_view.dart
+++ lib/View/top_loading_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
+import 'package:unicorn_flutter/Constants/strings.dart';
 import 'package:unicorn_flutter/Controller/top_loading_controller.dart';
 import 'package:unicorn_flutter/Service/Log/log_service.dart';
 import 'package:unicorn_flutter/View/Component/CustomWidget/custom_button.dart';
@@ -64,35 +65,52 @@ class TopLoadingView extends StatelessWidget {
                   },
                 ),
                 FutureBuilder(
-                    future: controller.checkReleaseBuild(),
-                    builder: (context, snapshot) {
-                      if (snapshot.hasData) {
-                        bool isReleaseBuild = snapshot.data!;
-                        if (isReleaseBuild) {
-                          return const SizedBox();
-                        } else {
-                          return Column(
-                            children: [
-                              const CustomText(
-                                text: 'DEBUG BUILD',
-                                color: ColorName.textGray,
-                                fontWeight: FontWeight.w300,
-                                fontSize: 12,
-                              ),
-                              const SizedBox(height: 8),
-                              CustomButton(
-                                text: 'データ初期化',
-                                onTap: () {
-                                  controller.clearData();
-                                },
-                              )
-                            ],
-                          );
-                        }
-                      } else {
+                  future: controller.checkReleaseBuild(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      bool isReleaseBuild = snapshot.data!;
+                      if (isReleaseBuild) {
                         return const SizedBox();
+                      } else {
+                        return Column(
+                          children: [
+                            const CustomText(
+                              text: 'DEBUG BUILD',
+                              color: ColorName.textGray,
+                              fontWeight: FontWeight.w300,
+                              fontSize: 12,
+                            ),
+                            const SizedBox(height: 8),
+                            CustomButton(
+                              text: 'データ初期化',
+                              onTap: () {
+                                controller.clearData();
+                              },
+                            )
+                          ],
+                        );
                       }
-                    }),
+                    } else {
+                      return const SizedBox();
+                    }
+                  },
+                ),
+                const SizedBox(height: 32),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    for (String message
+                        in Strings.TOP_LOADING_CAUTION_TEXT_LIST)
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 4),
+                        child: CustomText(
+                          text: message,
+                          fontSize: 10,
+                          color: ColorName.textGray,
+                        ),
+                      ),
+                  ],
+                ),
               ],
             ),
           ),
